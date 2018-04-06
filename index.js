cb2ca6cfaad15b4e49acf9da8399b4b7fd5e2f12
Emil Ajdyna <2088208+emilos@users.noreply.github.com>
2018-04-06 13:47:31 +0200

    Engine basics

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-"" 
+const { SAXParser } = require('parse5')
+
+function getValue (value, options) {
+    if (value.startsWith('${') && value.endsWith('}')) {
+        let property = value.substring(2, value.length - 1)
+        return options[property]
+    } else {
+        return value
+    }
+}
+
+function serialize (attrs, options, escape) {
+    let html = attrs.find(attr => attr.name === 'html')
+    if (html) {
+        return escape(getValue(html.value, options))
+    } else {
+        let text = attrs.find(attr => attr.name === 'text')
+        if (text) {
+            return getValue(text.value, options)
+        }
+    }
+    return ''
+}
+
+module.exports = {
+  render() {},
+  compile(source) {
+    const parser = new SAXParser()
+
+    return function (options, escape) {
+      let start = ''
+      let end = ''
+      parser.on('startTag', (node, attrs) => {
+        if (node === 'slot' && attrs) {
+            start += serialize(attrs, options, escape)
+        } else if (attrs) {
+            start += '<'
+            start += node
+            let allowed = attrs.filter(attr => attr.name !== 'html' && attr.name !== 'text')
+            if (allowed.length) {
+                start += ' '
+                start += allowed
+                    .filter(attr => attr.name !== 'html' && attr.name !== 'text')
+                    .map(attr => `${attr.name}="${attr.value}"`)
+                    .join(' ')
+            }
+            start += '>'
+            start += serialize(attrs, options, escape)
+        }
+      })
+      parser.on('endTag', node => {
+        if (node !== 'slot') {
+            end += `</${node}>`
+        }
+      })
+      parser.on('text', text => {
+        start += text
+      })
+      parser.write(`<slot>${source}</slot>`)
+      return start + end
+    }
+  }
+}
--- test.js
+++ test.js
@@ -1,1 +1,26 @@
-"" 
+const assert = require('assert')
+const { render, compile } = require('.')
+
+assert.deepEqual(compile('hello world')(), 'hello world')
+assert.deepEqual(compile('<div></div>')(), '<div></div>')
+assert.deepEqual(compile('<div>foo</div>')(), '<div>foo</div>')
+
+assert.deepEqual(compile('<input>')(), '<input>')
+assert.deepEqual(compile('<input type="number" value="100">')(), '<input type="number" value="100">')
+assert.deepEqual(compile('<input    value="100">')(), '<input value="100">')
+
+assert.deepEqual(compile('<slot text="foo"/>')(), 'foo')
+assert.deepEqual(compile('<slot html="foo"/>')({}, html => html.replace('foo', 'bar')), 'bar')
+
+assert.deepEqual(compile('<slot text="foo"></slot>')(), 'foo')
+assert.deepEqual(compile('<slot html="foo"></slot>')({}, html => html.replace('foo', 'bar')), 'bar')
+
+assert.deepEqual(compile('<slot text="${foo}"/>')({ foo: 'bar' }), 'bar')
+assert.deepEqual(compile('<slot html="${foo}"/>')({ foo: 'bar' }, html => html.replace('bar', 'foo')), 'foo')
+
+assert.deepEqual(compile('<slot text="${foo}"></slot>')({ foo: 'bar' }), 'bar')
+assert.deepEqual(compile('<slot html="${foo}"></slot>')({ foo: 'bar' }, html => html.replace('bar', 'foo')), 'foo')
+
+assert.deepEqual(compile('<div text="foo"></div>')(), '<div>foo</div>')
+assert.deepEqual(compile('<div html="foo"></div>')({}, html => html.replace('foo', 'bar')), '<div>bar</div>')
+
