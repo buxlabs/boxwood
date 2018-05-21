7e61a11bf703235eae5be1e98f3e46f8d2b40816
Emil Ajdyna <2088208+emilos@users.noreply.github.com>
2018-05-21 10:41:46 +0200

    Handle multiple actions

--- src/array.js
+++ src/array.js
@@ -1,22 +1,27 @@
-const ACTIONS = [ { alias: ['is', 'positive'], name: 'is_positive' }, { alias: ['is', 'negative'], name: 'is_negative' } ]
+const ACTIONS = [
+  { alias: ['is', 'positive'], name: 'is_positive' },
+  { alias: ['is', 'negative'], name: 'is_negative' }
+]
 
-function normalize (array) { //['foo', 'is', 'positive']
+function normalize (array) { //['foo', 'is', 'negative']
   const result = []
   let index
   for (let i = 0, ilen = array.length; i < ilen; i++) {
     let attribute = array[i]
     let found = false
-    let candidate = false
     index = i
     for (let j = 0, jlen = ACTIONS.length; j < jlen; j++) {
       let action = ACTIONS[j]
       if (action.alias[0] !== attribute) continue
-      candidate = true
       i++
       attribute = array[i]
       for (let k = 1, klen = action.alias.length; k < klen; k++) {
         let part = action.alias[k]
-        if (part !== attribute) break
+        if (part !== attribute) {
+          i = index
+          attribute = array[i]
+          break
+        }
         i++
         attribute = array[i]
         if (k === klen - 1) {
@@ -24,11 +29,8 @@ function normalize (array) { //['foo', 'is', 'positive']
           found = true
         }
       }
-      if (!found) {
-        i = index
-      }
     }
-    if (!found && !candidate) result.push(attribute)
+    if (!found) result.push(attribute)
   }
   return result
 }
--- src/array.spec.js
+++ src/array.spec.js
@@ -2,4 +2,4 @@ const assert = require('assert')
 const { normalize } = require('./array')
 
 assert.deepEqual(normalize(['foo', 'is', 'positive']), ['foo', 'is_positive'])
-// assert.deepEqual(normalize(['foo', 'is', 'negative']), ['foo', 'is_negative'])
+assert.deepEqual(normalize(['foo', 'is', 'negative']), ['foo', 'is_negative'])
