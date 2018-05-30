95d0d1278f1239bf3891b7d8a34d2067bcffac89
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2018-05-30 19:57:15 +0200

    Add failing fizzbuzz test

--- test/examples.js
+++ test/examples.js
@@ -4,12 +4,16 @@ const { equal } = require('assert')
 const { compile } = require('..')
 
 function test (name, data = {}) {
-  const fixture = join(__dirname, 'fixtures', name, 'input.html')
-  const template = readFileSync(fixture, 'utf8')
-  const fn = compile(template)
-  return compile(data, html => html)
+  const dir = join(__dirname, 'fixtures/examples')
+  const file1 = join(dir, name, 'actual.html')
+  const file2 = join(dir, name, 'expected.html')
+  const content1 = readFileSync(file1, 'utf8')
+  const template = compile(content1)
+  const actual = template(data, html => html)
+  const expected = readFileSync(file2, 'utf8')
+  equal(actual, expected)
 }
 
-// test('ad')
+// test('fizzbuzz')
 // test('simple')
 // test('footer')
