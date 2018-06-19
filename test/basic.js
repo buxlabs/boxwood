5b3e9949b5777efa775558f112d96b1c43ac2e05
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2018-06-19 11:03:42 +0200

    Added Date.parse feature

--- test/basic.js
+++ test/basic.js
@@ -1236,6 +1236,8 @@ equal(compile('{foo | prettydate}')({ foo: new Date(2018, 5, 29) }, html => html
 equal(compile('{foo | prettydate}')({ foo: new Date(2018, 5, 29) }, html => html), 'Friday, 29th of June 2018')
 equal(compile('<p>{foo | prettydate}</p>')({ foo: new Date(2018, 5, 29) }, html => html), '<p>Friday, 29th of June 2018</p>')
 equal(compile('<p>{new Date(2018, 5, 29) | prettydate}</p>')({}, html => html), '<p>Friday, 29th of June 2018</p>')
+equal(compile('{Date.parse("01 Jan 1970 00:00:00 GMT")}')({}, html => html), '0')
+equal(compile('{Date.parse("04 Dec 1995 00:12:00 GMT")}')({}, html => html), '818035920000')
 
 equal(compile('{foo | celsius}')({ foo: '70°F' }, html => html), '21°C')
 equal(compile('{foo | fahrenheit}')({ foo: '21°C' }, html => html), '70°F')
