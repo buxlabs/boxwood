ebff66a0fef6963da77d17e3a020dd8eee23146d
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2018-10-23 12:53:07 +0200

    Added handlebars to the benchmark

--- test/benchmark.js
+++ test/benchmark.js
@@ -2,6 +2,7 @@ const { Suite } = require('benchmark')
 const compile = require('./helpers/compile')
 const underscore = require('underscore')
 const template = require('lodash.template')
+const handlebars = require('handlebars')
 const mustache = require('mustache')
 const escape = require('escape-html')
 const { readFileSync } = require('fs')
@@ -11,13 +12,15 @@ const assert = require('assert')
 const source1 = readFileSync(path.join(__dirname, 'fixtures/benchmark/html-engine.html'), 'utf8')
 const source2 = readFileSync(path.join(__dirname, 'fixtures/benchmark/underscore.ejs'), 'utf8')
 const source3 = readFileSync(path.join(__dirname, 'fixtures/benchmark/lodash.ejs'), 'utf8')
-const source4 = readFileSync(path.join(__dirname, 'fixtures/benchmark/mustache.mst'), 'utf8')
+const source4 = readFileSync(path.join(__dirname, 'fixtures/benchmark/handlebars.hbs'), 'utf8')
+const source5 = readFileSync(path.join(__dirname, 'fixtures/benchmark/mustache.mst'), 'utf8')
 
 const suite = new Suite()
 const fn1 = compile(source1)
 const fn2 = underscore.template(source2)
 const fn3 = template(source3)
-mustache.parse(source4)
+const fn4 = handlebars.compile(source4)
+mustache.parse(source5)
 
 const data = {
   title: 'foo',
@@ -29,7 +32,6 @@ const data = {
   ]
 }
 
-
 function normalize (string) {
   return string.replace(/\s/g, '')
 }
@@ -37,7 +39,8 @@ const result = normalize(fn1(data, escape))
 
 assert.deepEqual(result, normalize(fn2(data)))
 assert.deepEqual(result, normalize(fn3(data)))
-assert.deepEqual(result, normalize(mustache.render(source4, data)))
+assert.deepEqual(result, normalize(fn4(data)))
+assert.deepEqual(result, normalize(mustache.render(source5, data)))
 
 suite.add('html-engine', function () {
   fn1(data, escape)
@@ -48,8 +51,11 @@ suite.add('html-engine', function () {
   .add('lodash', function () {
     fn3(data)
   })
+  .add('handlebars', function () {
+    fn4(source4, data)
+  })
   .add('mustache', function () {
-    mustache.render(source4, data)
+    mustache.render(source5, data)
   })
   .on('cycle', function (event) {
     console.log(String(event.target))
