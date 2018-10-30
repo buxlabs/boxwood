ec4e4b4a4e19b68444435673314bcfa3ad5d72c9
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2018-10-30 09:29:33 +0100

    Async engine

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const Compiler = require('./src/Compiler')
 module.exports = {
   compile (source, options = {}) {
     const compiler = new Compiler(options)
-    const trees = compiler.transform(compiler.parse(source))
-    return compiler.generate(trees)
+    let tree = compiler.parse(source)
+    return compiler.transform(tree)
+      .then(tree => {
+        return compiler.generate(tree)
+      })
   }
 }
