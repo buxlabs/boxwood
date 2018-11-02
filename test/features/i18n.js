6453fa9c6b661380acf04a2bbf10d64d2d135049
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2018-11-02 19:19:41 +0100

    Async test for i18n

--- test/features/i18n.js
+++ test/features/i18n.js
@@ -1,256 +1,301 @@
-const { equal, throws } = require('assert')
-const compile = require('../helpers/compile')
-const path = require('path')
+import test from 'ava'
+import compile from '../helpers/compile'
+import path from 'path'
 
-console.time('i18n')
+test('i18n', async assert => {
+  let template
+  console.time('i18n')
 
-equal(compile(`
-<script i18n>export default { submit: ['Wyślij', 'Send'] }</script>
-<div>{"submit" | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n>export default { submit: ['Wyślij', 'Send'] }</script><div>{"submit" | translate}</div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n>export default { submit: ['Wyślij', 'Send'] }</script>
-<div><translate submit /></div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n>export default { submit: ['Wyślij', 'Send'] }</script><div><translate submit /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n>export default { submit: ['Wyślij', 'Send'] }</script>
-<div>{"submit" | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'en' }, html => html), `<div>Send</div>`)
+  template = await compile(
+    `<script i18n>export default { submit: ['Wyślij', 'Send'] }</script><div>{"submit" | translate}</div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'en' }, html => html), '<div>Send</div>')
 
-equal(compile(`
-<script i18n>export default { submit: ['Wyślij', 'Send'] }</script>
-<div><translate submit /></div>
-`, { languages: ['pl', 'en'] })({ language: 'en' }, html => html), `<div>Send</div>`)
+  template = await compile(
+    `<script i18n>export default { submit: ['Wyślij', 'Send'] }</script><div>{"submit" | translate}</div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'en' }, html => html), '<div>Send</div>')
 
-equal(compile(`
-<script i18n>export default { 'submit': ['Wyślij', 'Send'] }</script>
-<div>{"submit" | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n>export default { submit: ['Wyślij', 'Send'] }</script><div><translate submit /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'en' }, html => html), '<div>Send</div>')
 
-equal(compile(`
-<script i18n>export default { 'submit': ['Wyślij', 'Send'] }</script>
-<div><translate submit /></div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n>export default { 'submit': ['Wyślij', 'Send'] }</script><div>{"submit" | translate}</div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n>export default { 'button.submit': ['Wyślij', 'Send'] }</script>
-<div>{"button.submit" | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n>export default { 'submit': ['Wyślij', 'Send'] }</script><div><translate submit /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n>export default { 'button.submit': ['Wyślij', 'Send'] }</script>
-<div><translate button.submit /></div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n>export default { 'button.submit': ['Wyślij', 'Send'] }</script><div>{"button.submit" | translate}</div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n>export default { submit: ['Wyślij', 'Send'] }</script>
-<div>{foo | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'pl', foo: 'submit' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n>export default { 'button.submit': ['Wyślij', 'Send'] }</script><div><translate button.submit /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n>export default { 'button.submit': ['Wyślij', 'Send'] }</script>
-<div>{foo | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'pl', foo: 'button.submit' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n>export default { submit: ['Wyślij', 'Send'] }</script><div>{foo | translate}</div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl', foo: 'submit' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n json>
-{
-  "submit": ["Wyślij", "Send"]
-}
-</script>
-<div>{"submit" | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n>export default { 'button.submit': ['Wyślij', 'Send'] }</script><div>{foo | translate}</div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl', foo: 'button.submit' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n json>
-{
-  "submit": ["Wyślij", "Send"]
-}
-</script>
-<div><translate submit /></div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n json>{"submit": ["Wyślij", "Send"]}</script><div>{"submit" | translate}</div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n json>
-{
-  "submit": ["Wyślij", "Send"]
-}
-</script>
-<div>{"submit" | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'en' }, html => html), `<div>Send</div>`)
+  template = await compile(
+    `<script i18n json>{"submit": ["Wyślij", "Send"]}</script><div><translate submit /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n json>
-{
-  "submit": ["Wyślij", "Send"]
-}
-</script>
-<div><translate submit /></div>
-`, { languages: ['pl', 'en'] })({ language: 'en' }, html => html), `<div>Send</div>`)
+  template = await compile(
+    `<script i18n json>{"submit": ["Wyślij", "Send"]}</script><div>{"submit" | translate}</div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'en' }, html => html), '<div>Send</div>')
 
-equal(compile(`
-<script i18n yaml>
-submit:
-- Wyślij
-- Send
-</script>
-<div>{"submit" | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n json>{"submit": ["Wyślij", "Send"]}</script><div><translate submit /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'en' }, html => html), '<div>Send</div>')
 
-equal(compile(`
-<script i18n yaml>
-submit:
-- Wyślij
-- Send
-</script>
-<div><translate submit /></div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n yaml>
+     submit:
+     - Wyślij
+     - Send
+    </script>
+    <div><translate submit /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n yaml>
-submit:
-- Wyślij
-- Send
-</script>
-<div>{"submit" | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'en' }, html => html), `<div>Send</div>`)
+  template = await compile(
+    `<script i18n yaml>
+     submit:
+     - Wyślij
+     - Send
+    </script>
+    <div><translate submit /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'en' }, html => html), '<div>Send</div>')
 
-equal(compile(`
-<script i18n yaml>
-submit:
-- Wyślij
-- Send
-</script>
-<div><translate submit /></div>
-`, { languages: ['pl', 'en'] })({ language: 'en' }, html => html), `<div>Send</div>`)
+  template = await compile(
+    `<script i18n yaml>
+     submit:
+     - Wyślij
+     - Send
+    </script>
+    <div><translate submit /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'en' }, html => html), '<div>Send</div>')
 
-equal(compile(`
-<script i18n yaml>
-button.submit:
-- Wyślij
-- Send
-</script>
-<div>{"button.submit" | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n yaml>
+     button.submit:
+     - Wyślij
+     - Send
+    </script>
+    <div>{"button.submit" | translate}</div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n yaml>
-button.submit:
-- Wyślij
-- Send
-</script>
-<div><translate button.submit /></div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
 
-equal(compile(`
-<script i18n yaml>
-button.submit:
-- Wyślij
-- Send
-</script>
-<div>{"button.submit" | translate}</div>
-`, { languages: ['pl', 'en'] })({ language: 'en' }, html => html), `<div>Send</div>`)
+  template = await compile(
+    `<script i18n yaml>
+     button.submit:
+     - Wyślij
+     - Send
+    </script>
+    <div><translate button.submit /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`
-<script i18n yaml>
-button.submit:
-- Wyślij
-- Send
-</script>
-<div><translate button.submit /></div>
-`, { languages: ['pl', 'en'] })({ language: 'en' }, html => html), `<div>Send</div>`)
+  template = await compile(
+    `<script i18n yaml>
+     button.submit:
+     - Wyślij
+     - Send
+    </script>
+    <div>{"button.submit" | translate}</div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'en' }, html => html), '<div>Send</div>')
 
-equal(compile(`<div><translate button.submit /></div>
-`, { languages: ['pl', 'en'], translationsPaths: [path.join(__dirname, '../fixtures/translations/translations.yaml')] })
-({ language: 'pl' }, html => html), `<div>Wyślij</div>`)
+  template = await compile(
+    `<script i18n yaml>
+     button.submit:
+     - Wyślij
+     - Send
+    </script>
+    <div><translate button.submit /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'en' }, html => html), '<div>Send</div>')
 
-equal(compile(`
-<script i18n json>
-{
-  "submit": ["Wyślij Wiadomość", "Send the message"]
-}
-</script>
-<div><translate submit /></div>
-`, { languages: ['pl', 'en'], translationsPaths: [path.join(__dirname, '../fixtures/translations/translations.json')] })
-({ language: 'en' }, html => html), `<div>Send the message</div>`)
+  template = await compile(
+    `<div><translate button.submit /></div>`,
+    { languages: ['pl', 'en'], translationsPaths: [path.join(__dirname, '../fixtures/translations/translations.yaml')] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Wyślij</div>')
 
-equal(compile(`<div><translate cancel /></div>`, {
-  languages: ['pl', 'en'], translationsPaths: [path.join(__dirname, '../fixtures/translations/translations.json')]
-})({ language: 'pl' }, html => html), `<div>anuluj</div>`)
+  template = await compile(
+    `<script i18n json>{"submit": ["Wyślij Wiadomość", "Send the message"]}</script><div><translate submit /></div>`,
+    { languages: ['pl', 'en'], translationsPaths: [path.join(__dirname, '../fixtures/translations/translations.json')] }
+  )
+  assert.deepEqual(template({ language: 'en' }, html => html), '<div>Send the message</div>')
 
-equal(compile(`
-<i18n yaml>
-hello:
-- 'Cześć'
-- 'Hello'
-</i18n>
-<div><translate hello /></div>
-`, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html), `<div>Cześć</div>`)
+  template = await compile(
+    `<div><translate cancel /></div>`,
+    { languages: ['pl', 'en'], translationsPaths: [path.join(__dirname, '../fixtures/translations/translations.json')] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>anuluj</div>')
 
-throws(function () {
-  compile(`<div><translate cancel /></div>`, {
-    languages: ['pl', 'en'],
-    translationsPaths: [
-      path.join(__dirname, '../fixtures/translations/translations.json'),
-      path.join(__dirname, '../fixtures/translations/locales.json'),
-    ]
-  })({ language: 'pl' }, html => html)
-}, /Translation already exists in .*/)
+  template = await compile(
+    `<i18n yaml>
+     hello:
+     - 'Cześć'
+     - 'Hello'
+     </i18n>
+    <div><translate hello /></div>`,
+    { languages: ['pl', 'en'] }
+  )
+  assert.deepEqual(template({ language: 'pl' }, html => html), '<div>Cześć</div>')
 
-throws(function () {
-  compile(`
-  <script i18n>export default { submit: ['Wyślij', 'Send'] }</script>
-  <div>{"submit" | translate}</div>
-  `, {})
-}, /Compiler option is undefined: languages\./)
+  try {
+    template = await compile('<div><translate cancel /></div>', {
+      languages: ['pl', 'en'],
+      translationsPaths: [
+        path.join(__dirname, '../fixtures/translations/translations.json'),
+        path.join(__dirname, '../fixtures/translations/locales.json'),
+      ]
+    })
+    template({ language: 'pl' }, html => html)
+  }
+  catch (error) {
+    assert.regex(error.message, /Translation already exists in .*/)
+  }
 
-throws(function () {
-  compile(`
-  <script i18n>export default { submit: ['Wyślij', 'Send'] }</script>
-  <div><translate submit /></div>
-  `, {})
-}, /Compiler option is undefined: languages\./)
+  try {
+    template = await compile(`<script i18n>export default { submit: ['Wyślij', 'Send'] }</script><div>{"submit" | translate}</div>`, {})
+  } catch (error) {
+    assert.regex(error.message, /Compiler option is undefined: languages\./)
+  }
 
-throws(function () {
-  compile(`
-  <script i18n yaml>
-  submit:
-  - Wyślij
-  - Send
-  </script>
-  <div><translate copyright /></div>
-  `, { languages: ['pl', 'en'] })({ language: 'pl' }, html => html)
-}, /There is no translation for the copyright key/)
+  try {
+    await compile(`<script i18n>export default { submit: ['Wyślij', 'Send'] }</script><div><translate submit /></div>`, {})
+  } catch (error) {
+    assert.regex(error.message, /Compiler option is undefined: languages\./)
+  }
 
-throws(function () {
-  compile(`
-  <script i18n yaml>
-  copyright:
-  - Wszystkie prawa zastrzeżone
-  </script>
-  <div><translate copyright /></div>
-  `, { languages: ['pl', 'en'] })({ language: 'en' }, html => html)
-}, /There is no translation for the copyright key in en language/)
+  try {
+    await compile(`<script i18n>export default { submit: ['Wyślij', 'Send'] }</script><div><translate submit /></div>`, {})
+  } catch (error) {
+    assert.regex(error.message, /Compiler option is undefined: languages\./)
+  }
 
-throws(function () {
-  compile(`
-  <script i18n yaml>
-  copyright:
-  - Wszystkie prawa zastrzeżone
-  - All rights reserved
-  </script>
-  <div><translate contact /></div>
-  `, { languages: ['pl', 'en'] })({ language: 'en' }, html => html)
-}, /There is no translation for the contact key/)
+  try {
+    template = await compile(`
+      <script i18n yaml>
+      submit:
+      - Wyślij
+      - Send
+      </script>
+      <div><translate copyright /></div>`,
+      { languages: ['pl', 'en'] }
+    )
+    template({ language: 'pl' }, html => html)
+  } catch (error) {
+    assert.regex(error.message, /There is no translation for the copyright key/)
+  }
 
-throws(function () {
-  compile(`<script i18n></script><div>{"foo" | translate}</div>)`, { languages: ['pl', 'en'] })({ language: 'en' }, html => html)
-}, /The translation script cannot be empty/)
+  try {
+    template = await compile(`
+      <script i18n yaml>
+      copyright:
+      - Wszystkie prawa zastrzeżone
+      </script>
+      <div><translate copyright /></div>`,
+      { languages: ['pl', 'en'] }
+    )
+    template({ language: 'en' }, html => html)
+  } catch (error) {
+    assert.regex(error.message, /There is no translation for the copyright key in en language/)
+  }
 
-throws(function () {
-  compile(`<script i18n yaml></script><div>{"foo" | translate}</div>)`, { languages: ['pl', 'en'] })({ language: 'en' }, html => html)
-}, /The translation script cannot be empty/)
+  try {
+    template = await compile(`
+      <script i18n yaml>
+      copyright:
+      - Wszystkie prawa zastrzeżone
+      - All rights reserved
+      </script>
+      <div><translate contact /></div>`,
+      { languages: ['pl', 'en'] }
+    )
+    template({ language: 'en' }, html => html)
+  } catch (error) {
+    assert.regex(error.message, /There is no translation for the contact key/)
+  }
 
-console.timeEnd('i18n')
+  try {
+    template = await compile(`<script i18n></script><div>{"foo" | translate}</div>`,
+      { languages: ['pl', 'en'] }
+    )
+    template({ language: 'en' }, html => html)
+  } catch (error) {
+    assert.regex(error.message, /The translation script cannot be empty/)
+  }
+
+  try {
+    template = await compile(`<script i18n yaml></script><div>{"foo" | translate}</div>)`,
+      { languages: ['pl', 'en'] }
+    )
+    template({ language: 'en' }, html => html)
+  } catch (error) {
+    assert.regex(error.message, /The translation script cannot be empty/)
+  }
+
+  console.timeEnd('i18n')
+})
