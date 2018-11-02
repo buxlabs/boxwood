ba8d949b6d3fa6ed0c3cee1388a70993d5a99e15
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2018-11-02 19:35:44 +0100

    Test for operators

--- test/features/operators.js
+++ test/features/operators.js
@@ -1,18 +1,40 @@
-const { equal } = require('assert')
-const compile = require('../helpers/compile')
+import test from 'ava'
+import compile from '../helpers/compile'
 
-console.time('ternary')
+test('ternary', async assert => {
+  let template
+  console.time('ternary')
 
-equal(compile('{foo ? "bar" : "baz"}')({ foo: true }, html => html), 'bar')
-equal(compile('{foo ? "bar" : "baz"}')({ foo: false }, html => html), 'baz')
-equal(compile('{foo ? bar : baz}')({ foo: true, bar: 'bar', baz: 'baz' }, html => html), 'bar')
-equal(compile('{foo ? bar : baz}')({ foo: false, bar: 'bar', baz: 'baz' }, html => html), 'baz')
-equal(compile('{foo.bar ? bar : baz}')({ foo: { bar: true }, bar: 'bar', baz: 'baz' }, html => html), 'bar')
-equal(compile('{foo.bar ? bar : baz}')({ foo: { bar: false }, bar: 'bar', baz: 'baz' }, html => html), 'baz')
-equal(compile('{foo.bar ? bar.baz : baz.qux}')({ foo: { bar: true }, bar: { baz: 'bar' }, baz: { qux: 'baz' } }, html => html), 'bar')
-equal(compile('{foo.bar ? bar.baz : baz.qux}')({ foo: { bar: false }, bar: { baz: 'bar' }, baz: { qux: 'baz' } }, html => html), 'baz')
-equal(compile('{foo || "bar"}')({ foo: 'foo' }, html => html), 'foo')
-equal(compile('{foo || "bar"}')({ foo: '' }, html => html), 'bar')
+  template = await compile('{foo ? "bar" : "baz"}')
+  assert.deepEqual(template({ foo: true }, html => html), 'bar')
 
-// TODO {foo[bar] ? ...}
-console.timeEnd('ternary')
+  template = await compile('{foo ? "bar" : "baz"}')
+  assert.deepEqual(template({ foo: false }, html => html), 'baz')
+
+  template = await compile('{foo ? bar : baz}')
+  assert.deepEqual(template({ foo: true, bar: 'bar', baz: 'baz' }, html => html), 'bar')
+
+  template = await compile('{foo ? bar : baz}')
+  assert.deepEqual(template({ foo: false, bar: 'bar', baz: 'baz' }, html => html), 'baz')
+
+  template = await compile('{foo.bar ? bar : baz}')
+  assert.deepEqual(template({ foo: { bar: true }, bar: 'bar', baz: 'baz' }, html => html), 'bar')
+
+  template = await compile('{foo.bar ? bar : baz}')
+  assert.deepEqual(template({ foo: { bar: false }, bar: 'bar', baz: 'baz' }, html => html), 'baz')
+
+  template = await compile('{foo.bar ? bar.baz : baz.qux}')
+  assert.deepEqual(template({ foo: { bar: true }, bar: { baz: 'bar' }, baz: { qux: 'baz' } }, html => html), 'bar')
+
+  template = await compile('{foo.bar ? bar.baz : baz.qux}')
+  assert.deepEqual(template({ foo: { bar: false }, bar: { baz: 'bar' }, baz: { qux: 'baz' } }, html => html), 'baz')
+
+  template = await compile('{foo || "bar"}')
+  assert.deepEqual(template({ foo: 'foo' }, html => html), 'foo')
+
+  template = await compile('{foo || "bar"}')
+  assert.deepEqual(template({ foo: '' }, html => html), 'bar')
+
+  // TODO {foo[bar] ? ...}
+  console.timeEnd('ternary')
+})
