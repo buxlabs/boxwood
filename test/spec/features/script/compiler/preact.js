fd512c2593033bf7b6d421090d4993ac56f0d52d
Emil Ajdyna <2088208+emilos@users.noreply.github.com>
2018-11-22 13:44:08 +0100

    Add a test example with preact integration

--- test/spec/features/script/compiler/preact.js
+++ test/spec/features/script/compiler/preact.js
@@ -1,5 +1,65 @@
 import test from 'ava'
+import compile from '../../../../helpers/compile'
+import { normalize } from '../../../../helpers/string'
+import { rollup } from 'rollup'
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import { readFileSync, writeFileSync, unlinkSync } from 'fs'
+import { tmpdir } from 'os'
+import { join } from 'path'
 
 test('script: preact', async assert => {
-  assert.truthy(true)
+  let template
+  console.time('script: preact')
+
+  template = await compile(`
+    <div id='app'></div>
+    <script compiler="preact">
+      import { render } from 'preact'
+      const Foo = ({ bar }) => {
+        return (<span>{name}</span>)
+      }
+      render(
+        <Foo bar="baz" />,
+        document.getElementById('app')
+      )
+    </script>
+  `, {
+    compilers: {
+      preact: async (source, options) => {
+        const input = join(tmpdir(), 'rollup.js')
+        writeFileSync(input, source)
+        const bundle = await rollup({
+          input,
+          plugins: [
+            babel({
+              plugins: [
+                ['@babel/plugin-transform-react-jsx', { 'pragma': 'h' }]
+              ]
+            }),
+            resolve({
+              module: true,
+              jsnext: true,
+              main: true,
+              browser: true,
+              customResolveOptions: {
+                moduleDirectory: join(__dirname, '../../../../../node_modules')
+              }
+            }),
+            commonjs()
+          ]
+        })
+        unlinkSync(input)
+        const { code } = await bundle.generate({
+          format: 'iife'
+        })
+        return code
+      }
+    }
+  })
+
+  assert.deepEqual(normalize(template({}, html => html)), normalize(`<div id="app"></div><script>` + readFileSync(join(__dirname, '../../../../fixtures/preact', 'expected.js'), 'utf8') + `</script>`))
+
+  console.timeEnd('script: preact')
 })
