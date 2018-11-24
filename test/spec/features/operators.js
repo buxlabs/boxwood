f814a37acb007fcc3374ed9ad64bd5f08f9c7295
Emil Ajdyna <2088208+emilos@users.noreply.github.com>
2018-11-24 21:59:36 +0100

    Add more specs

--- test/spec/features/operators.js
+++ test/spec/features/operators.js
@@ -1,7 +1,63 @@
 import test from '../../helpers/test'
 import compile from '../../helpers/compile'
 
-test('ternary', async assert => {
+test('operators: logical and', async assert => {
+  let template
+
+  template = await compile('<div>{foo && bar}</div>')
+  assert.deepEqual(template({ foo: true, bar: true }, html => html), '<div>true</div>')
+
+  template = await compile('<div>{foo && bar}</div>')
+  assert.deepEqual(template({ foo: true, bar: false }, html => html), '<div>false</div>')
+
+  template = await compile('<div>{foo && bar}</div>')
+  assert.deepEqual(template({ foo: false, bar: true }, html => html), '<div>false</div>')
+
+  template = await compile('<div>{foo && bar}</div>')
+  assert.deepEqual(template({ foo: false, bar: false }, html => html), '<div>false</div>')
+})
+
+test('operators: logical or', async assert => {
+  let template
+
+  template = await compile('<div>{foo || bar}</div>')
+  assert.deepEqual(template({ foo: true, bar: true }, html => html), '<div>true</div>')
+
+  template = await compile('<div>{foo || bar}</div>')
+  assert.deepEqual(template({ foo: true, bar: false }, html => html), '<div>true</div>')
+
+  template = await compile('<div>{foo || bar}</div>')
+  assert.deepEqual(template({ foo: false, bar: true }, html => html), '<div>true</div>')
+
+  template = await compile('<div>{foo || bar}</div>')
+  assert.deepEqual(template({ foo: false, bar: false }, html => html), '<div>false</div>')
+})
+
+test('operators: comparison', async assert => {
+  let template
+
+  template = await compile('<div>{foo > bar}</div>')
+  assert.deepEqual(template({ foo: 1, bar: 0 }, html => html), '<div>true</div>')
+
+  template = await compile('<div>{foo < bar}</div>')
+  assert.deepEqual(template({ foo: 1, bar: 0 }, html => html), '<div>false</div>')
+
+  template = await compile('<div>{foo >= bar}</div>')
+  assert.deepEqual(template({ foo: 1, bar: 0 }, html => html), '<div>true</div>')
+
+  template = await compile('<div>{foo <= bar}</div>')
+  assert.deepEqual(template({ foo: 1, bar: 0 }, html => html), '<div>false</div>')
+
+  template = await compile('{foo.length > 0 ? "active" : "inactive"}')
+  assert.deepEqual(template({ foo: ['bar'] }, html => html), 'active')
+  assert.deepEqual(template({ foo: [] }, html => html), 'inactive')
+
+  // TODO handle more complex expressions
+  // template = await compile('<div>{foo > bar && baz > qux}</div>')
+  // assert.deepEqual(template({ foo: 1, bar: 0, baz: 1, qux: 0 }, html => html), '<div>true</div>')
+})
+
+test('operators: ternary', async assert => {
   let template
 
   template = await compile('{foo ? "bar" : "baz"}')
