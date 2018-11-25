c8208b5006d4fed52a55ca7141c3743d1ff4387e
Emil Ajdyna <2088208+emilos@users.noreply.github.com>
2018-11-25 20:30:52 +0100

    Bring back benchmarks

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,66 +1,72 @@
-const { Suite } = require('benchmark')
-const compile = require('./helpers/compile')
-const underscore = require('underscore')
-const template = require('lodash.template')
-const handlebars = require('handlebars')
-const mustache = require('mustache')
-const escape = require('escape-html')
-const { readFileSync } = require('fs')
-const path = require('path')
-const assert = require('assert')
+import test from 'ava'
+import { Suite } from 'benchmark'
+import compile from './helpers/compile'
+import { readFile } from './helpers/fs'
+import underscore from 'underscore'
+import template from 'lodash.template'
+import handlebars from 'handlebars'
+import mustache from 'mustache'
+import escape from 'escape-html'
+import path from 'path'
 
-const source1 = readFileSync(path.join(__dirname, 'fixtures/benchmark/html-engine.html'), 'utf8')
-const source2 = readFileSync(path.join(__dirname, 'fixtures/benchmark/underscore.ejs'), 'utf8')
-const source3 = readFileSync(path.join(__dirname, 'fixtures/benchmark/lodash.ejs'), 'utf8')
-const source4 = readFileSync(path.join(__dirname, 'fixtures/benchmark/handlebars.hbs'), 'utf8')
-const source5 = readFileSync(path.join(__dirname, 'fixtures/benchmark/mustache.mst'), 'utf8')
+test('benchmark', async assert => {
+  const source1 = await readFile(path.join(__dirname, 'fixtures/benchmark/html-engine.html'))
+  const source2 = await readFile(path.join(__dirname, 'fixtures/benchmark/underscore.ejs'))
+  const source3 = await readFile(path.join(__dirname, 'fixtures/benchmark/lodash.ejs'))
+  const source4 = await readFile(path.join(__dirname, 'fixtures/benchmark/handlebars.hbs'))
+  const source5 = await readFile(path.join(__dirname, 'fixtures/benchmark/mustache.mst'))
 
-const suite = new Suite()
-const fn1 = compile(source1)
-const fn2 = underscore.template(source2)
-const fn3 = template(source3)
-const fn4 = handlebars.compile(source4)
-mustache.parse(source5)
+  const suite = new Suite()
+  const fn1 = await compile(source1)
+  const fn2 = underscore.template(source2)
+  const fn3 = template(source3)
+  const fn4 = handlebars.compile(source4)
+  mustache.parse(source5)
 
-const data = {
-  title: 'foo',
-  subtitle: 'baz',
-  todos: [
-    { description: 'lorem ipsum' },
-    { description: 'dolor sit' },
-    { description: 'amet' }
-  ]
-}
+  const data = {
+    title: 'foo',
+    subtitle: 'baz',
+    todos: [
+      { description: 'lorem ipsum' },
+      { description: 'dolor sit' },
+      { description: 'amet' }
+    ]
+  }
 
-function normalize (string) {
-  return string.replace(/\s/g, '')
-}
-const result = normalize(fn1(data, escape))
+  function normalize (string) {
+    return string.replace(/\s/g, '')
+  }
+  const result = normalize(fn1(data, escape))
 
-assert.deepEqual(result, normalize(fn2(data)))
-assert.deepEqual(result, normalize(fn3(data)))
-assert.deepEqual(result, normalize(fn4(data)))
-assert.deepEqual(result, normalize(mustache.render(source5, data)))
+  assert.deepEqual(result, normalize(fn2(data)))
+  assert.deepEqual(result, normalize(fn3(data)))
+  assert.deepEqual(result, normalize(fn4(data)))
+  assert.deepEqual(result, normalize(mustache.render(source5, data)))
 
-suite.add('pure-engine', function () {
-  fn1(data, escape)
+  await new Promise(resolve => {
+    suite.add('pure-engine', function () {
+      fn1(data, escape)
+    })
+    .add('underscore', function () {
+      fn2(data)
+    })
+    .add('lodash', function () {
+      fn3(data)
+    })
+    .add('handlebars', function () {
+      fn4(source4, data)
+    })
+    .add('mustache', function () {
+      mustache.render(source5, data)
+    })
+    .on('cycle', function (event) {
+      console.log(String(event.target))
+    })
+    .on('complete', function () {
+      console.log('Fastest is ' + this.filter('fastest').map('name'))
+      resolve()
+    })
+    .run({ 'async': true })
+  })
 })
-  .add('underscore', function () {
-    fn2(data)
-  })
-  .add('lodash', function () {
-    fn3(data)
-  })
-  .add('handlebars', function () {
-    fn4(source4, data)
-  })
-  .add('mustache', function () {
-    mustache.render(source5, data)
-  })
-  .on('cycle', function (event) {
-    console.log(String(event.target))
-  })
-  .on('complete', function () {
-    console.log('Fastest is ' + this.filter('fastest').map('name'))
-  })
-  .run({ 'async': true })
+
