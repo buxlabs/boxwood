26040b31ec49fc3c902d283611f3a70367e42726
Emil Ajdyna <2088208+emilos@users.noreply.github.com>
2019-01-17 09:36:42 +0100

    Remove useless file

--- src/html/stringify.js
+++
@@ -1,6 +0,0 @@
-const { stringify } = require('himalaya')
-const defaults = require('./defaults')
-
-module.exports = function (source, options) {
-  return stringify(source, defaults(source, options))
-}
