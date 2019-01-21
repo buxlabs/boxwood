d6220c4e1266fbdbd7d70b43b2b525b5b09bc74c
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2019-01-21 12:21:58 +0100

    Support simple expressions

--- src/plugins/PaddingPlugin.js
+++ src/plugins/PaddingPlugin.js
@@ -1,23 +1,30 @@
+const { isCurlyTag, getExpressionFromCurlyTag } = require('../string')
 const serialize = require('asttv')
-const AbstractSyntaxTree = require('abstract-syntax-tree')
 const { isNumeric } = require('pure-conditions')
+const AbstractSyntaxTree = require('abstract-syntax-tree')
 
 class PaddingPlugin {
+  getValue (attribute) {
+    let value = attribute.value.replace(/\s+/g, '')
+    if (isCurlyTag(value)) {
+      value = getExpressionFromCurlyTag(value)
+      const { expression } = new AbstractSyntaxTree(value).body[0]
+      return expression.value
+    }
+    return value
+  }
+
   prepare ({ keys, fragment, attrs }) {
     const padding = attrs.find(attr => attr.key === 'padding')
     const style = attrs.find(attr => attr.key === 'style')
     const index = attrs.findIndex(attr => attr.key === 'padding')
-
     if (style) {
-    } else if (padding) {
-      fragment.attributes.splice(index, 1)
-      if (padding.value !== '0' && isNumeric(padding.value)) {
-        fragment.attributes.push({ key: 'style', value: `${padding.key}: ${padding.value}px;` })
-      } else {
-        fragment.attributes.push({ key: 'style', value: `${padding.key}: ${padding.value};` })
-      }
-    } else {
 
+    } else if (padding) {
+      let value = this.getValue(padding)
+      fragment.attributes.splice(index, 1)
+      const suffix = (isNumeric(value) && Number(value) !== 0) ? 'px' : ''
+      fragment.attributes.push({ key: 'style', value: `${padding.key}: ${value}${suffix};` })
     }
   }
   run () {}
--- test/spec/attributes/padding.js
+++ test/spec/attributes/padding.js
@@ -2,35 +2,38 @@ import test from '../../helpers/test'
 import compile from '../../helpers/compile'
 import escape from 'escape-html'
 
-test('div[padding]: simple passing a value', async assert => {
-  const template = await compile(`<div padding="15px"></div>`)
+test('div[padding]: working with values', async assert => {
+  let template = await compile(`<div padding="15px"></div>`)
   assert.deepEqual(template({}, escape), '<div style="padding: 15px;"></div>')
-})
 
-test('div[padding]: working with another attributes', async assert => {
-  const template = await compile(`<div id="qux" class="foo bar ban" padding="15px"></div>`)
+  template = await compile(`<div id="qux" class="foo bar ban" padding="15px"></div>`)
   assert.deepEqual(template({}, escape), '<div id="qux" class="foo bar ban" style="padding: 15px;"></div>')
-})
 
-test('div[padding]: working with many attributes', async assert => {
-  const template = await compile(`
-    <div class="row" padding="0">
-      <div id="qux" class="foo bar ban" padding="15px"></div>
-    </div>`)
+  template = await compile(`<div class="row" padding="0"><div id="qux" class="foo bar ban" padding="15px"></div></div>`)
   assert.deepEqual(template({}, escape), '<div class="row" style="padding: 0;"><div id="qux" class="foo bar ban" style="padding: 15px;"></div></div>')
-})
 
-test('div[padding]: working without unit', async assert => {
-  const template = await compile(`<div id="qux" class="foo bar ban" padding="15"></div>`)
+  template = await compile(`<div id="qux" class="foo bar ban" padding="15"></div>`)
   assert.deepEqual(template({}, escape), '<div id="qux" class="foo bar ban" style="padding: 15px;"></div>')
-})
 
-test('div[padding]: working with units', async assert => {
-  const template = await compile(`<div id="qux" class="foo bar ban" padding="15%"></div>`)
+  template = await compile(`<div id="qux" class="foo bar ban" padding="15%"></div>`)
   assert.deepEqual(template({}, escape), '<div id="qux" class="foo bar ban" style="padding: 15%;"></div>')
+
+  template = await compile(`<div id="qux" class="foo bar ban" padding="3em"></div>`)
+  assert.deepEqual(template({}, escape), '<div id="qux" class="foo bar ban" style="padding: 3em;"></div>')
 })
 
-test('div[padding]: working with units2', async assert => {
-  const template = await compile(`<div id="qux" class="foo bar ban" padding="3em"></div>`)
-  assert.deepEqual(template({}, escape), '<div id="qux" class="foo bar ban" style="padding: 3em;"></div>')
+test('div[padding]: working with expressions', async assert => {
+  let template = await compile(`<div id="qux" class="foo bar ban" padding="{15}"></div>`)
+  assert.deepEqual(template({}, escape), '<div id="qux" class="foo bar ban" style="padding: 15px;"></div>')
+
+  template = await compile(`<div id="qux" class="foo bar ban" padding="{0}"></div>`)
+  assert.deepEqual(template({}, escape), '<div id="qux" class="foo bar ban" style="padding: 0;"></div>')
+
+  template = await compile(`<div id="qux" class="foo bar ban" padding="{'15px'}"></div>`)
+  assert.deepEqual(template({}, escape), '<div id="qux" class="foo bar ban" style="padding: 15px;"></div>')
+
+  template = await compile(`<div id="qux" class="foo bar ban" padding="{'  15px  '}"></div>`)
+  assert.deepEqual(template({}, escape), '<div id="qux" class="foo bar ban" style="padding: 15px;"></div>')
+
+  await assert.throwsAsync(compile(`<div id="qux" class="foo bar ban" padding="{15px}"></div>`))
 })
