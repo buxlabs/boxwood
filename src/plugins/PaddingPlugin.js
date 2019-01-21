77f7e672ddfc077a23105dae5c600cacfbabd1e2
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2019-01-21 14:31:39 +0100

    Dummy implementation for object support

--- src/plugins/PaddingPlugin.js
+++ src/plugins/PaddingPlugin.js
@@ -1,30 +1,57 @@
 const { isCurlyTag, getExpressionFromCurlyTag } = require('../string')
 const { isNumeric } = require('pure-conditions')
 const AbstractSyntaxTree = require('abstract-syntax-tree')
+const serialize = require('asttv')
 
 class PaddingPlugin {
   convertValue (value) {
     value = value.replace(/\s+/g, '')
     if (isCurlyTag(value)) {
       value = getExpressionFromCurlyTag(value)
-      const { expression } = new AbstractSyntaxTree(value).body[0]
+      const { expression } = new AbstractSyntaxTree(`(${value})`).body[0]
+      if (expression.type === 'ObjectExpression') {
+        const object = serialize(expression)
+        return object
+      }
       return expression.value
     }
     return value
+  }
+
+  stringify (object) {
+    let result = ``
+    let space = ' '
+    for (let prop in object) {
+      result += `${prop}: ${object[prop]}${space}`
+    }
+    result = result.trim()
+    return result
   }
 
   prepare ({ keys, fragment, attrs }) {
     const attribute = attrs.find(attr => attr.key === 'padding')
     if (attribute) {
       let { key, value } = attribute
-      value = this.convertValue(value)
-      const suffix = (isNumeric(value) && Number(value) !== 0) ? 'px' : ''
-      fragment.attributes.splice(attrs.findIndex(attr => attr.key === 'padding'), 1)
-      if (attrs.find(attr => attr.key === 'style')) {
-        const styleIndex = fragment.attributes.findIndex(attr => attr.key === 'style')
-        fragment.attributes[styleIndex].value += ` ${key}: ${value}${suffix};`
+      value = this.convertValue(attribute.value)
+      if (typeof value === 'object') {
+        const padding = {}
+        let suffix = ''
+        for (let key in value) {
+          suffix = (isNumeric(value[key]) && Number(value[key]) !== 0) ? 'px' : ''
+          padding[`${attribute.key}-${key}`] = `${value[key]}${suffix};`
+        }
+        fragment.attributes.splice(attrs.findIndex(attr => attr.key === 'padding'), 1)
+        const result = this.stringify(padding)
+        fragment.attributes.push({ key: 'style', value: result })
       } else {
-        fragment.attributes.push({ key: 'style', value: `${key}: ${value}${suffix};` })
+        const suffix = (isNumeric(value) && Number(value) !== 0) ? 'px' : ''
+        fragment.attributes.splice(attrs.findIndex(attr => attr.key === 'padding'), 1)
+        if (attrs.find(attr => attr.key === 'style')) {
+          const styleIndex = fragment.attributes.findIndex(attr => attr.key === 'style')
+          fragment.attributes[styleIndex].value += ` ${key}: ${value}${suffix};`
+        } else {
+          fragment.attributes.push({ key: 'style', value: `${key}: ${value}${suffix};` })
+        }
       }
     }
   }
--- test/spec/attributes/padding.js
+++ test/spec/attributes/padding.js
@@ -2,7 +2,7 @@ import test from '../../helpers/test'
 import compile from '../../helpers/compile'
 import escape from 'escape-html'
 
-test('div[padding]: working with values', async assert => {
+test('div[padding]: works with values', async assert => {
   let template = await compile(`<div padding="15px"></div>`)
   assert.deepEqual(template({}, escape), '<div style="padding: 15px;"></div>')
 
@@ -31,7 +31,7 @@ test('div[padding]: working with values', async assert => {
   assert.deepEqual(template({}, escape), '<div style="margin: 0 auto; background: #f0f; padding: 15px;"><h1>Hello World!</h1></div>')
 })
 
-test('div[padding]: working with expressions', async assert => {
+test('div[padding]: works with expressions', async assert => {
   let template = await compile(`<div id="qux" class="foo bar ban" padding="{15}"></div>`)
   assert.deepEqual(template({}, escape), '<div id="qux" class="foo bar ban" style="padding: 15px;"></div>')
 
@@ -57,4 +57,30 @@ test('div[padding]: working with expressions', async assert => {
 
   template = await compile(`<div padding="{ '15px' }" style="margin: 0 auto;"></div>`)
   assert.deepEqual(template({}, escape), '<div style="margin: 0 auto; padding: 15px;"></div>')
+})
+
+test('div[padding]: works with object', async assert => {
+  let template = await compile(`<div padding="{{ top: 15 }}"></div>`)
+  assert.deepEqual(template({}, escape), '<div style="padding-top: 15px;"></div>')
+
+  template = await compile(`<div padding="{{ top: 15, left: 30 }}"></div>`)
+  assert.deepEqual(template({}, escape), '<div style="padding-top: 15px; padding-left: 30px;"></div>')
+
+  template = await compile(`<div padding="{{ top: 15, left: 30, right: 0 }}"></div>`)
+  assert.deepEqual(template({}, escape), '<div style="padding-top: 15px; padding-left: 30px; padding-right: 0;"></div>')
+
+  template = await compile(`<div padding="{{ top: 15, left: 30, right: 0, bottom: 100 }}"></div>`)
+  assert.deepEqual(template({}, escape), '<div style="padding-top: 15px; padding-left: 30px; padding-right: 0; padding-bottom: 100px;"></div>')
+
+  template = await compile(`<div padding="{{ top: '15px' }}"></div>`)
+  assert.deepEqual(template({}, escape), '<div style="padding-top: 15px;"></div>')
+
+  template = await compile(`<div padding="{{ top: '15px', left: '30px' }}"></div>`)
+  assert.deepEqual(template({}, escape), '<div style="padding-top: 15px; padding-left: 30px;"></div>')
+
+  template = await compile(`<div padding="{{ top: '15px', left: '30px', right: 0 }}"></div>`)
+  assert.deepEqual(template({}, escape), '<div style="padding-top: 15px; padding-left: 30px; padding-right: 0;"></div>')
+
+  template = await compile(`<div padding="{{ top: '15px', left: '30px', right: 0, bottom: '100px' }}"></div>`)
+  assert.deepEqual(template({}, escape), '<div style="padding-top: 15px; padding-left: 30px; padding-right: 0; padding-bottom: 100px;"></div>')
 })
