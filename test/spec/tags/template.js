b018e3832ad6407080a8015a4ac63403a96fd6b6
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2019-02-08 10:33:43 +0100

    Prevent change node name for objects

--- test/spec/tags/template.js
+++ test/spec/tags/template.js
@@ -27,3 +27,14 @@ test('template: objects as parameters', async assert => {
   `)
   assert.deepEqual(template({}, escape), 'qux')
 })
+
+test('template: padding attribute', async assert => {
+  let template = await compile(`<template section><div padding="{{ bottom: 30 }}"><p>Inline component</p></div></template><section></section>`)
+  assert.deepEqual(template({}, escape), '<div style="padding-bottom: 30px;"><p>Inline component</p></div>')
+
+  template = await compile(`<template section><div padding="{{ bottom: "30", top: "150" }}"><p>Inline component</p></div></template><section></section>`)
+  assert.deepEqual(template({}, escape), '<div style="padding-bottom: 30px; padding-top: 150px;"><p>Inline component</p></div>')
+
+  template = await compile(`<template section><div margin="{{ top: 100, bottom: 100, right: 100, left: 100 }}"><p>Inline component</p></div></template><section></section>`)
+  assert.deepEqual(template({}, escape), '<div style="margin-top: 100px; margin-bottom: 100px; margin-right: 100px; margin-left: 100px;"><p>Inline component</p></div>')
+})
