2398f11f1575eaf12ce884aa6c45b7be288ed60d
Emil Ajdyna <2088208+emilos@users.noreply.github.com>
2019-02-21 21:31:02 +0100

    Walk through imports instead of having a regexp

--- src/html/defaults.js
+++ src/html/defaults.js
@@ -1,13 +1,45 @@
-const { parseDefaults } = require('himalaya')
+const { parse, parseDefaults } = require('himalaya')
 const { VOID_TAGS } = require('../enum')
+const walk = require('himalaya-walk')
 
-function regexp (tag, name) {
-  return new RegExp(`<${tag}\\s+${name}\\s+`)
+function matchImportTags (source) {
+  return source.match(/<import[\s\S]*?>[\s\S]*?>/gi)
+}
+
+function getImportTags (source) {
+  const imports = matchImportTags(source)
+  if (!imports) return []
+  return imports.join('')
+}
+
+function getTags (attributes) {
+  const { key } = attributes[0]
+  // TODO support other syntax, e.g.
+  // <import foo,bar from="./components" />
+  // <import foo, bar from="./components" />
+  // <import {foo,bar} from="./components" />
+  // <import { foo,bar} from="./components" />
+  // <import {foo,bar } from="./components" />
+  // <import { foo,bar } from="./components" />
+  // <import { foo, bar } from="./components" />
+  // <import { foo , bar } from="./components" />
+  return [key]
 }
 
 function deduceVoidTags (source) {
+  const tags = []
+  const imports = getImportTags(source)
+  const tree = parse(imports)
+  walk(tree, node => {
+    if (node.tagName === 'import' || node.tagName === 'require') {
+      getTags(node.attributes).forEach(tag => {
+        tags.push(tag)
+      })
+    }
+  })
+
   return VOID_TAGS.concat(parseDefaults.voidTags).filter(name => {
-    return !regexp('import', name).test(source) && !regexp('require', name).test(source)
+    return !tags.includes(name)
   })
 }
 
