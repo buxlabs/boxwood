2e69508b83bdf5c208ec7cda9fb3e9d93ae2e15f
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2019-04-03 15:18:15 +0200

    Support camelCase and kebab-case attributes

--- test/spec/attributes/border.js
+++ test/spec/attributes/border.js
@@ -3,6 +3,15 @@ import compile from '../../helpers/compile'
 import escape from 'escape-html'
 
 test('div[border]: works with values', async assert => {
-  const template = await compile(`<div border="1px solid red"></div>`)
+  let template = await compile(`<div border="1px solid red"></div>`)
   assert.deepEqual(template({}, escape), '<div style="border: 1px solid red;"></div>')
+
+  template = await compile(`<div border-left="1px solid red"></div>`)
+  assert.deepEqual(template({}, escape), '<div style="border-left: 1px solid red;"></div>')
+
+  template = await compile(`<div borderLeft="1px solid red"></div>`)
+  assert.deepEqual(template({}, escape), '<div style="border-left: 1px solid red;"></div>')
+
+  template = await compile(`<div border-top="2px" border-bottom="2px"></div>`)
+  assert.deepEqual(template({}, escape), '<div style="border-top: 2px; border-bottom: 2px;"></div>')
 })
