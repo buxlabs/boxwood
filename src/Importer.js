c338f4dbca1a78173e9915d9b6d3b24284560f4a
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2019-04-26 10:10:07 +0200

    Remote nested components

--- src/Importer.js
+++ src/Importer.js
@@ -6,15 +6,17 @@ const { flatten } = require('pure-utilities/collection')
 const Linter = require('./Linter')
 const { isRemotePath } = require('./files')
 const request = require('axios')
+const URL = require('url')
 
 const { getComponentNames, getAssetPaths, getImportNodes } = require('./node')
 const parse = require('./html/parse')
 const linter = new Linter()
 
-async function loadComponent (path, paths = []) {
-  if (isRemotePath(path)) {
+async function loadComponent (path, isRemote, remoteUrl, paths = []) {
+  if (isRemotePath(path) || isRemote) {
     try {
-      const response = await request.get(path)
+      const url = remoteUrl ? URL.parse(remoteUrl).protocol + '//' + join(URL.parse(remoteUrl).host, dirname(URL.parse(remoteUrl).pathname), path) : path
+      const response = await request.get(url)
       if (response.status === 200) {
         let buffer = Buffer.from('') // TODO: parse response to the buffer
         let base64 = '' // TODO: find a good way to change data to base64, like readFile
@@ -22,7 +24,9 @@ async function loadComponent (path, paths = []) {
           path,
           source: response.data,
           buffer,
-          base64
+          base64,
+          remote: true,
+          url
         }
       }
     } catch (exception) {}
@@ -36,6 +40,7 @@ async function loadComponent (path, paths = []) {
         result.base64 = await readFile(location, 'base64')
         // TODO: Read once convert base64
         result.source = await readFile(location, 'utf8')
+        result.remote = false
         return result
       } catch (exception) {}
     }
@@ -43,7 +48,7 @@ async function loadComponent (path, paths = []) {
   return {}
 }
 
-async function fetch (node, kind, context, options) {
+async function fetch (node, kind, context, isRemote, remoteUrl, options) {
   const paths = options.paths || []
   const names = kind === 'IMPORT' ? getComponentNames(node) : ['']
   return Promise.all(names.map(async name => {
@@ -51,7 +56,7 @@ async function fetch (node, kind, context, options) {
     const dir = dirname(context)
     const assetPaths = getAssetPaths(node, name)
     return Promise.all(assetPaths.map(async assetPath => {
-      const { source, path, base64, buffer } = await loadComponent(assetPath, [dir, ...paths])
+      const { source, path, base64, remote, url, buffer } = await loadComponent(assetPath, isRemote, remoteUrl, [dir, ...paths])
       if (!path) {
         return {
           warnings: [{ type: 'COMPONENT_NOT_FOUND', message: `Component not found: ${name}` }]
@@ -60,12 +65,12 @@ async function fetch (node, kind, context, options) {
       const tree = parse(source)
       const files = [context]
       const warnings = []
-      return { name, source, base64, buffer, path, files, warnings, tree, type }
+      return { name, source, base64, remote, url, buffer, path, files, warnings, tree, type }
     }))
   }))
 }
 const MAXIMUM_IMPORT_DEPTH = 50
-async function recursiveImport (tree, source, path, options, depth) {
+async function recursiveImport (tree, source, path, options, depth, remote, url) {
   if (depth > MAXIMUM_IMPORT_DEPTH) {
     return {
       assets: [],
@@ -74,10 +79,10 @@ async function recursiveImport (tree, source, path, options, depth) {
   }
   const imports = getImportNodes(tree, options)
   const warnings = linter.lint(tree, source, imports.map(({ node }) => node))
-  const assets = await Promise.all(imports.map(({ node, kind }) => fetch(node, kind, path, options)))
+  const assets = await Promise.all(imports.map(({ node, kind }) => fetch(node, kind, path, remote, url, options)))
   const current = flatten(assets)
   const nested = await Promise.all(current.filter(element => element.tree).map(async element => {
-    return recursiveImport(element.tree, element.source, element.path, options, depth + 1)
+    return recursiveImport(element.tree, element.source, element.path, options, depth + 1, element.remote, element.url)
   }))
   let nestedAssets = current.concat(flatten(nested.map(object => object.assets)))
   nestedAssets = mergeAssets(nestedAssets)
@@ -108,6 +113,6 @@ module.exports = class Importer {
     this.options = options
   }
   async import () {
-    return recursiveImport(this.tree, this.source, '.', this.options, 0)
+    return recursiveImport(this.tree, this.source, '.', this.options, 0, false, null)
   }
 }
