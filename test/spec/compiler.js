f51078d9d1b4f772b248fa436fa90cc0ae285d60
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2019-05-16 14:30:21 +0200

    Simple compiler option validation

--- test/spec/compiler.js
+++ test/spec/compiler.js
@@ -1,7 +1,100 @@
 import test from 'ava'
 import compile from '../helpers/compile'
 
-test('basic: it returns nothing for no template', async assert => {
+test('compiler: returns errors for invalid paths option', async assert => {
   var { errors } = await compile('', { paths: 'foo' })
-  assert.deepEqual(errors[0], 'Compiler option paths must be an array')
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'Compiler option "paths" must be an array')
+
+  var { errors } = await compile('', { paths: [null, 447] })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'Compiler option "paths" must contain only strings')
+
+  var { errors } = await compile('', { paths: ['foo/bar', ''] })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'Compiler option "paths" cannot contains empty strings')
+
+  var { errors } = await compile('', { paths: [__dirname, './foo/bar/baz'] })
+  assert.deepEqual(errors, [])  
+})
+
+test('compiler: returns errors for invalid inline options', async assert => {
+  var { errors } = await compile('', { inline: 'foo' })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'Compiler option inline must be an array')
+
+  var { errors } = await compile('', { inline: ['images', 'fonts', 'scripts'] })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'Inline option must be one of ["images", "scripts", "stylesheets"]')
+  
+  var { errors } = await compile('', { inline: ['images', 'scripts'] })
+  assert.deepEqual(errors, [])    
+
+  var { errors } = await compile('', { inline: [] })
+  assert.deepEqual(errors, [])
+})
+
+test('compiler: returns errors for invalid compilers options', async assert => {
+  var { errors } = await compile('', { compilers: [] })
+  assert.deepEqual(errors.length, 1)
+  assert.deepEqual(errors[0], 'Compiler option "compilers" must be an object')
+  
+  var { errors } = await compile('', { compilers: null })
+  assert.deepEqual(errors.length, 1)
+  assert.deepEqual(errors[0], 'Compiler option "compilers" must be an object')
+  
+  var { errors } = await compile('', { compilers: { foo: 'foo' } })
+  assert.deepEqual(errors.length, 1)
+  assert.deepEqual(errors[0], 'Compiler option "compilers" must contain only functions')
+
+  var { errors } = await compile('', { compilers: { foo () {} }})
+  assert.deepEqual(errors, [])
+})
+
+test('compiler: returns errors for invalid languages option', async assert => {
+  var { errors } = await compile('', { languages: 'foo' })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'Compiler option "languages" must be an array')
+
+  var { errors } = await compile('', { languages: [null, 447] })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'Compiler option "languages" must contain only strings')
+  
+  var { errors } = await compile('', { languages: ['pl', 'en'] })
+  assert.deepEqual(errors, [])  
+})
+
+test('compiler: returns errors for invalid cache option', async assert => {
+  var { errors } = await compile('', { cache: 'foo' })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'Compiler option "cache" must be an boolean')
+  
+  var { errors } = await compile('', { cache: true })
+  assert.deepEqual(errors, [])  
+})
+
+test('compiler: returns errors for invalid aliases option', async assert => {
+  var { errors } = await compile('', { aliases: 'foo' })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'Compiler option "aliases" must be an array')
+
+  var { errors } = await compile('', { aliases: ['foo'] })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'Compiler option "aliases" must be an array containing objects')  
+  
+  var { errors } = await compile('', { aliases: [{}] })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'An alias cannot be an empty object')
+  
+  var { errors } = await compile('', { aliases: [{ foo: '', bar: '' }] })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'An alias must have "from" and "to" property')
+  
+  var { errors } = await compile('', { aliases: [{ from: '', to: '' }] })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'An alias "from" option must be an regexp')
+     
+  var { errors } = await compile('', { aliases: [{ from: /baz/, to: null }] })
+  assert.deepEqual(errors.length, 1) 
+  assert.deepEqual(errors[0], 'An alias "to" option must be an string')  
 })
