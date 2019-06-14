89de6e31b5ca8442cee312112bf4820b009d0d28
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2019-06-14 16:02:47 +0200

    Replaced node assertion with ava

--- src/Component/curlyTagReduction.spec.js
+++ src/Component/curlyTagReduction.spec.js
@@ -1,48 +1,50 @@
-const assert = require('assert')
-const curlyTagReduction = require('./curlyTagReduction')
+import test from 'ava'
+import curlyTagReduction from './curlyTagReduction'
 
-assert.deepEqual(curlyTagReduction('{foo}', []), '')
-assert.deepEqual(curlyTagReduction('{foo || ""}', []), '')
-assert.deepEqual(curlyTagReduction('{foo || "bar"}', []), 'bar')
+test('curlyTagReduction: simplifies expressions in curly tags', assert => {
+  assert.deepEqual(curlyTagReduction('{foo}', []), '')
+  assert.deepEqual(curlyTagReduction('{foo || ""}', []), '')
+  assert.deepEqual(curlyTagReduction('{foo || "bar"}', []), 'bar')
 
-assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: 'bar' }]), 'bar')
-assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: 'bar' }]), 'bar')
-assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: 'bar' }]), 'bar')
+  assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: 'bar' }]), 'bar')
+  assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: 'bar' }]), 'bar')
+  assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: 'bar' }]), 'bar')
 
-assert.deepEqual(curlyTagReduction('{foo || bar}', []), '')
-assert.deepEqual(curlyTagReduction('{foo || bar}', [{ key: 'foo', value: 'foo' }]), 'foo')
-assert.deepEqual(curlyTagReduction('{foo || bar}', [{ key: 'bar', value: 'bar' }]), 'bar')
-assert.deepEqual(curlyTagReduction('{foo || bar}', [{ key: 'foo', value: 'foo' }, { key: 'bar', value: 'bar' }]), 'foo')
+  assert.deepEqual(curlyTagReduction('{foo || bar}', []), '')
+  assert.deepEqual(curlyTagReduction('{foo || bar}', [{ key: 'foo', value: 'foo' }]), 'foo')
+  assert.deepEqual(curlyTagReduction('{foo || bar}', [{ key: 'bar', value: 'bar' }]), 'bar')
+  assert.deepEqual(curlyTagReduction('{foo || bar}', [{ key: 'foo', value: 'foo' }, { key: 'bar', value: 'bar' }]), 'foo')
 
-assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: '{foo}' }]), '{foo}')
-assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: '{foo}' }]), '{foo || ""}')
-assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: '{foo}' }]), '{foo || "bar"}')
+  assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: '{foo}' }]), '{foo}')
+  assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: '{foo}' }]), '{foo || ""}')
+  assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: '{foo}' }]), '{foo || "bar"}')
 
-assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: '{foo.bar}' }]), '{foo.bar}')
-assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: '{foo.bar}' }]), '{foo.bar || ""}')
-assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: '{foo.bar}' }]), '{foo.bar || "bar"}')
+  assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: '{foo.bar}' }]), '{foo.bar}')
+  assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: '{foo.bar}' }]), '{foo.bar || ""}')
+  assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: '{foo.bar}' }]), '{foo.bar || "bar"}')
 
-assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: '{foo[bar]}' }]), '{foo[bar]}')
-assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: '{foo[bar]}' }]), '{foo[bar] || ""}')
-assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: '{foo[bar]}' }]), '{foo[bar] || "bar"}')
+  assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: '{foo[bar]}' }]), '{foo[bar]}')
+  assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: '{foo[bar]}' }]), '{foo[bar] || ""}')
+  assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: '{foo[bar]}' }]), '{foo[bar] || "bar"}')
 
-// TODO: Ensure that is needed
-assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: '{true}' }]), '{true}')
-assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: '{true}' }]), '{true}')
-assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: '{true}' }]), '{true}')
+  // TODO: Ensure that is needed
+  assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: '{true}' }]), '{true}')
+  assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: '{true}' }]), '{true}')
+  assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: '{true}' }]), '{true}')
 
-assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: '{false}' }]), '{false}')
-assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: '{false}' }]), '')
-assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: '{false}' }]), 'bar')
+  assert.deepEqual(curlyTagReduction('{foo}', [{ key: 'foo', value: '{false}' }]), '{false}')
+  assert.deepEqual(curlyTagReduction('{foo || ""}', [{ key: 'foo', value: '{false}' }]), '')
+  assert.deepEqual(curlyTagReduction('{foo || "bar"}', [{ key: 'foo', value: '{false}' }]), 'bar')
 
-assert.deepEqual(curlyTagReduction('{foo}{bar}', []), '')
-assert.deepEqual(curlyTagReduction('{foo} {bar}', []), ' ')
+  assert.deepEqual(curlyTagReduction('{foo}{bar}', []), '')
+  assert.deepEqual(curlyTagReduction('{foo} {bar}', []), ' ')
 
-assert.deepEqual(curlyTagReduction('<div class="{foo}"></div>', []), `<div></div>`)
-assert.deepEqual(curlyTagReduction('<div class="{foo}"></div>', [{ key: 'foo', value: '' }]), `<div></div>`)
-assert.deepEqual(curlyTagReduction('<div class="{foo}"></div>', [{ key: 'foo', value: '  ' }]), `<div class='  '></div>`)
-assert.deepEqual(curlyTagReduction('<div class="{foo}"></div>', [{ key: 'foo', value: undefined }]), `<div class='{undefined}'></div>`)
-assert.deepEqual(curlyTagReduction('<div class="{foo}"></div>', [{ key: 'foo', value: null }]), `<div class='{null}'></div>`)
-assert.deepEqual(curlyTagReduction('<div padding="{{bottom:30}}"></div>', []), `<div padding='{({\n  bottom: 30\n})}'></div>`)
+  assert.deepEqual(curlyTagReduction('<div class="{foo}"></div>', []), `<div></div>`)
+  assert.deepEqual(curlyTagReduction('<div class="{foo}"></div>', [{ key: 'foo', value: '' }]), `<div></div>`)
+  assert.deepEqual(curlyTagReduction('<div class="{foo}"></div>', [{ key: 'foo', value: '  ' }]), `<div class='  '></div>`)
+  assert.deepEqual(curlyTagReduction('<div class="{foo}"></div>', [{ key: 'foo', value: undefined }]), `<div class='{undefined}'></div>`)
+  assert.deepEqual(curlyTagReduction('<div class="{foo}"></div>', [{ key: 'foo', value: null }]), `<div class='{null}'></div>`)
+  assert.deepEqual(curlyTagReduction('<div padding="{{bottom:30}}"></div>', []), `<div padding='{({\n  bottom: 30\n})}'></div>`)
 
-assert.deepEqual(curlyTagReduction('<style>.foo { color: red }</style>', []), `<style>.foo { color: red }</style>`)
+  assert.deepEqual(curlyTagReduction('<style>.foo { color: red }</style>', []), `<style>.foo { color: red }</style>`)
+})
--- src/array.spec.js
+++ src/array.spec.js
@@ -1,29 +1,31 @@
-const assert = require('assert')
-const { normalize } = require('./array')
+import test from 'ava'
+import { normalize } from './array'
 
-assert.deepEqual(normalize([
-  { key: 'not', value: null }, { key: 'foo', value: null }
-]), [{ key: 'not', value: null, type: 'Action' }, { key: 'foo', value: null, type: 'Identifier' }])
+test('normalize: sets proper type for action and identifiers', assert => {
+  assert.deepEqual(normalize([
+    { key: 'not', value: null }, { key: 'foo', value: null }
+  ]), [{ key: 'not', value: null, type: 'Action' }, { key: 'foo', value: null, type: 'Identifier' }])
 
-assert.deepEqual(normalize([
-  { key: 'foo', value: null },
-  { key: 'is', value: null },
-  { key: 'positive', value: null }
-]), [
-  { key: 'foo', value: null, type: 'Identifier' },
-  { key: 'is_positive', value: null, type: 'Action' }
-])
+  assert.deepEqual(normalize([
+    { key: 'foo', value: null },
+    { key: 'is', value: null },
+    { key: 'positive', value: null }
+  ]), [
+    { key: 'foo', value: null, type: 'Identifier' },
+    { key: 'is_positive', value: null, type: 'Action' }
+  ])
 
-assert.deepEqual(normalize([
-  { key: 'foo', value: null },
-  { key: 'is', value: null },
-  { key: 'less', value: null },
-  { key: 'than', value: null },
-  { key: 'or', value: null },
-  { key: 'equals', value: null },
-  { key: 'bar', value: null }
-]), [
-  { key: 'foo', value: null, type: 'Identifier' },
-  { key: 'is_less_than_or_equals', value: null, type: 'Action' },
-  { key: 'bar', value: null, type: 'Identifier' }
-])
+  assert.deepEqual(normalize([
+    { key: 'foo', value: null },
+    { key: 'is', value: null },
+    { key: 'less', value: null },
+    { key: 'than', value: null },
+    { key: 'or', value: null },
+    { key: 'equals', value: null },
+    { key: 'bar', value: null }
+  ]), [
+    { key: 'foo', value: null, type: 'Identifier' },
+    { key: 'is_less_than_or_equals', value: null, type: 'Action' },
+    { key: 'bar', value: null, type: 'Identifier' }
+  ])
+})
