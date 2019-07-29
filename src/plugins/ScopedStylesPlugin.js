172c2533922344305d027dee194b6f9ce1c77afd
Emil Ajdyna <emil@ajdyna.com>
2019-07-29 21:54:24 +0200

    Make scoped styles more robust

--- src/plugins/ScopedStylesPlugin.js
+++ src/plugins/ScopedStylesPlugin.js
@@ -101,25 +101,26 @@ function addClassAttributeWithScopeToHtmlTag (tag, attributes, scopes) {
 }
 
 class ScopedStylesPlugin extends Plugin {
+  constructor () {
+    super()
+    this.scopes = {}
+  }
   beforeprerun () {
-    this.scopes = []
+    this.scopes[this.depth] = []
   }
   prerun ({ tag, keys, children, attributes }) {
     if (tag === 'style' && keys.includes('scoped')) {
-      children.forEach(node => addScopeToCssSelectors(node, this.scopes, attributes))
+      children.forEach(node => addScopeToCssSelectors(node, this.scopes[this.depth], attributes))
     }
   }
   run ({ tag, keys, attributes }) {
-    if (this.scopes.length > 0) {
+    if (this.scopes[this.depth].length > 0) {
       if (keys && keys.includes('class')) {
-        addScopeToHtmlClassAttribute(tag, attributes, this.scopes)
+        addScopeToHtmlClassAttribute(tag, attributes, this.scopes[this.depth])
       } else {
-        addClassAttributeWithScopeToHtmlTag(tag, attributes, this.scopes)
+        addClassAttributeWithScopeToHtmlTag(tag, attributes, this.scopes[this.depth])
       }
     }
-  }
-  afterrun () {
-    this.scopes = []
   }
 }
 
