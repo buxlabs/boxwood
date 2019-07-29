fa4819f32873c181f95ea52d49556ce228029368
pkonieczniak <przemyslaw.konieczniak@gmail.com>
2019-07-29 12:46:57 +0200

    Simplify code

--- src/tags/entity.js
+++ src/tags/entity.js
@@ -1,56 +1,15 @@
 const { getTemplateAssignmentExpression } = require('../factory')
 const { getLiteral } = require('../ast')
-
-const ENTITIES = [
-  {
-    value: ' ',
-    identifier: ['nbsp', '#160', 'non-breaking space']
-  },
-  {
-    value: '<',
-    identifier: ['lt', '#60', 'less than']
-  },
-  {
-    value: '>',
-    identifier: ['gt', '#62', 'greater than']
-  },
-  {
-    value: '&',
-    identifier: ['amp', '#38', 'ampersand']
-  },
-  {
-    value: '"',
-    identifier: ['quot', '#34', 'double quotation mark']
-  },
-  {
-    value: '\'',
-    identifier: ['apos', '#39', 'single quotation mark']
-  },
-  {
-    value: '¢',
-    identifier: ['cent', '#162']
-  },
-  {
-    value: '£',
-    identifier: ['pound', '#163']
-  },
-  {
-    value: '¥',
-    identifier: ['yen', '#165']
-  },
-  {
-    value: '€',
-    identifier: ['euro', '#8364']
-  },
-  {
-    value: '©',
-    identifier: ['copy', '#169', 'copyright']
-  },
-  {
-    value: '®',
-    identifier: ['reg', '#174', 'registered trademark']
-  }
-]
+const ALIASES = {
+  'non-breaking space': ' ',
+  'less than': '<',
+  'greater than': '>',
+  'ampersand': '&',
+  'double quotation mark': '"',
+  'single quotation mark': '\'',
+  'copyright': '©',
+  'registered trademark': '®'
+}
 
 module.exports = function ({ tree, options, attrs }) {
   let identifier = attrs.reduce((accumulator, attribute) => {
@@ -58,10 +17,6 @@ module.exports = function ({ tree, options, attrs }) {
     return accumulator
   }, '')
   identifier = identifier.trim()
-  for (let i = 0; i < ENTITIES.length; i++) {
-    const entity = ENTITIES[i]
-    if (entity.identifier.includes(identifier)) {
-      return tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral(entity.value)))
-    }
-  }
+  const literal = getLiteral(ALIASES[identifier] || `&${identifier};`)
+  return tree.append(getTemplateAssignmentExpression(options.variables.template, literal))
 }
