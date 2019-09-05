c467890db69aa40fbcec3cd2e7d1ea033edad3b6
Emil Ajdyna <emil@ajdyna.com>
2019-09-05 09:53:23 +0200

    Split attributeToStyle into two methods

--- src/css.js
+++ src/css.js
@@ -1,14 +1,17 @@
+function addAttribute (attrs, key, value) {
+  const index = attrs.findIndex(attr => attr.key === key)
+  if (index === -1) {
+    attrs.push({ key, value })
+  } else {
+    attrs[index].value = `${value} ${attrs[index].value}`
+  }
+}
+
 function attributeToStyle (attrs, attributeNames, styles) {
   const index = attrs.findIndex(attr => attributeNames.includes(attr.key))
   if (index !== -1) {
     attrs.splice(index, 1)
-    const styleAttributeIndex = attrs.findIndex(attr => attr.key === 'style')
-    if (styleAttributeIndex === -1) {
-      attrs.push({ key: 'style', value: styles })
-    } else {
-      const { value } = attrs[styleAttributeIndex]
-      attrs[styleAttributeIndex].value = `${styles} ${value}`
-    }
+    addAttribute(attrs, 'style', styles)
   }
   return attrs
 }
