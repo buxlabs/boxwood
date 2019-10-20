c41cb4a158caeea3e756ce4c44a02676d7b571ba
Emil Ajdyna <emil@ajdyna.com>
2019-10-20 22:33:25 +0200

    Add a failing spec

--- test/spec/browser/counter.js
+++ test/spec/browser/counter.js
@@ -1,9 +1,9 @@
 import test from 'ava'
 import puppeteer from 'puppeteer'
 import Server from '../../helpers/Server'
-import { expect } from 'cherow';
+import compile from '../../helpers/compile'
 
-test('counter', async assert => {
+test('counter: vanilla', async assert => {
   const server = new Server()
   const { port } = await server.start()
   server.get('/', (request, response) => {
@@ -23,6 +23,35 @@ test('counter', async assert => {
   const page = await browser.newPage()
   await page.goto(`http://localhost:${port}`)
   var content = await page.content()
+  assert.truthy(content.includes('Clicked 0 times'))
+  await page.click("button")
+  var content = await page.content()
+  assert.truthy(content.includes('Clicked 1 time'))
+  await browser.close()
+  await server.stop()
+})
+
+test.skip('counter: scoped', async assert => {
+  const server = new Server()
+  const { port } = await server.start()
+  server.get('/', async (request, response) => {
+    const { template } = await compile(`
+      <button onclick={onClick}>Clicked {count} {count === 1 ? 'time' : 'times'}</button>
+      <script scoped>
+        let count = 0;
+        function onClick () {
+          count += 1
+        }
+      </script>
+    `, {})
+    console.log(template.toString())
+    response.send(template())
+  })
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(`http://localhost:${port}`)
+  var content = await page.content()
+  console.log(content)
   assert.truthy(content.includes('Clicked 0 times'))
   await page.click("button")
   var content = await page.content()
