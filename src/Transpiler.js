472aaa33f5e45bbeb041002af716f0b8703b1904
Emil Ajdyna <emil@ajdyna.com>
2019-12-14 23:04:55 +0100

    Basics of the transpiler

--- src/Transpiler.js
+++ src/Transpiler.js
@@ -1,6 +1,56 @@
-class Transpiler {
-  transpile () {
+const Lexer = require('html-lexer')
 
+const tokenize = (source) => {
+  const tokens = []
+  const delegate = {
+    write: (token) => tokens.push(token),
+    end: () => null
+  }
+  const lexer = new Lexer(delegate)
+  lexer.write(source)
+  lexer.end()
+  return tokens
+}
+
+const transform = (tokens) => {
+  const output = []
+  let context = null
+  tokens.forEach((token, index) => {
+    const [type, value] = token
+    if (type === 'tagName') {
+      if (value === 'if' || value === 'unless') {
+        context = value
+      } else if (value === 'elseif' || value === 'else' || value === 'elseunless') {
+        if (context === 'if' || context === 'elseif' || context === 'unless') {
+          const last = output.pop()
+          output.push(['beginEndTag', '</'])
+          output.push(['tagName', context])
+          output.push(['finishTag', '>'])
+          output.push(last)
+          context = value
+        }
+      } else if (value === 'end') {
+        if (context === 'if' || context === 'unless' || context === 'elseunless' || context === 'else' || context === 'elseif') {
+          tokens[index - 1][0] = 'beginEndTag'
+          tokens[index - 1][1] = '</'
+          token[1] = context
+        }
+        context = null
+      }
+    }
+    output.push(token)
+  })
+  return output
+}
+
+const stringify = (tokens) => {
+  return tokens.map(([type, value]) => value).join('')
+}
+
+class Transpiler {
+  transpile (source) {
+    const tokens = tokenize(source)
+    return stringify(transform(tokens))
   }
 }
 
--- src/Transpiler.spec.js
+++ src/Transpiler.spec.js
@@ -1,8 +1,52 @@
 import test from 'ava'
 import Transpiler from './Transpiler'
 
-test.skip('it converts <end> for an <if> tag', assert => {
+const transpile = (source) => {
   const transpiler = new Transpiler()
-  const source = transpiler.transpile('<if foo><end>')
-  assert.deepEqual(source, '<if foo></if>')
+  return transpiler.transpile(source)
+}
+
+test('it transpiles <end> for an <if> tag', assert => {
+  const source = transpile('<if foo>bar<end>')
+  assert.deepEqual(source, '<if foo>bar</if>')
+})
+
+test('it transpiles </end> for an <if> tag', assert => {
+  const source = transpile('<if foo>bar</end>')
+  assert.deepEqual(source, '<if foo>bar</if>')
+})
+
+test('it transpiles <end> for an <if> and <else> tags', assert => {
+  const source = transpile('<if foo>bar<else>baz<end>')
+  assert.deepEqual(source, '<if foo>bar</if><else>baz</else>')
+})
+
+test('it transpiles <end> for an <if> and <elseif> tags', assert => {
+  const source = transpile('<if foo>bar<elseif baz>qux<elseif quux>quuux<end>')
+  assert.deepEqual(source, '<if foo>bar</if><elseif baz>qux</elseif><elseif quux>quuux</elseif>')
+})
+
+test('it transpiles <end> for an <if>, <elseif> and <else> tags', assert => {
+  const source = transpile('<if foo>bar<elseif baz>qux<else>quux<end>')
+  assert.deepEqual(source, '<if foo>bar</if><elseif baz>qux</elseif><else>quux</else>')
+})
+
+test('it transpiles <end> for an <unless> tag', assert => {
+  const source = transpile('<unless foo>bar<end>')
+  assert.deepEqual(source, '<unless foo>bar</unless>')
+})
+
+test('it transpiles <end> for an <unless> and <elseunless> tag', assert => {
+  const source = transpile('<unless foo>bar<elseunless>baz<end>')
+  assert.deepEqual(source, '<unless foo>bar</unless><elseunless>baz</elseunless>')
+})
+
+test.skip('it works correctly for nested statements', assert => {
+  const source = transpile('<if foo><if bar>baz<end><end>')
+  assert.deepEqual(source, '<if foo><if bar>baz</if></if>')
+})
+
+test('it does not create duplicate closing tags', assert => {
+  const source = transpile('<if foo>bar</if>')
+  assert.deepEqual(source, '<if foo>bar</if>')
 })
