a21bdcd18a0c610add44daad7ab8251c860eca8f
Emil Ajdyna <emil@ajdyna.com>
2019-12-22 22:08:58 +0100

    Move translate tag code to a separate file

---
+++ src/tags/translate.js
@@ -0,0 +1,9 @@
+const { getTemplateAssignmentExpression } = require('../utilities/factory')
+const { getTranslateCallExpression } = require('../utilities/translations')
+
+module.exports = function translate ({ tree, attrs, options, variables, filters, translations, languages }) {
+  const attribute = attrs[0]
+  const { key } = attribute
+  const expression = getTranslateCallExpression(key, variables, filters, translations, languages)
+  tree.append(getTemplateAssignmentExpression(options.variables.template, expression))
+}
