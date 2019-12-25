b422a72b6717f0060e98136be7c0e1f8599557e5
Emil Ajdyna <emil@ajdyna.com>
2019-12-25 23:08:02 +0100

    Update self closing tags list

--- src/utilities/enum.js
+++ src/utilities/enum.js
@@ -44,7 +44,8 @@ module.exports = {
   SELF_CLOSING_TAGS: [
     'area', 'base', 'br', 'col', 'command',
     'embed', 'hr', 'img', 'input', 'keygen', 'link',
-    'meta', 'param', 'source', 'track', 'wbr', '!doctype'
+    'meta', 'param', 'source', 'track', 'wbr', '!doctype',
+    'if', 'else', 'elseif', 'unless', 'elseunless', 'end'
   ],
   SPECIAL_TAGS: [
     'if',
