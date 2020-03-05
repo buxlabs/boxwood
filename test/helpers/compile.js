f61851d0bffe005097dd972f2ed7f802e23c440c
Emil Ajdyna <emil@ajdyna.com>
2020-03-05 22:48:23 +0100

    Add DEBUG in tests

--- test/helpers/compile.js
+++ test/helpers/compile.js
@@ -2,6 +2,9 @@ const { compile } = require('../..')
 
 module.exports = async function () {
   return compile.apply(this, arguments).then(({ template, statistics, errors, warnings }) => {
+    if (process.env.DEBUG === 'pure-engine') {
+      console.log(template.toString())
+    }
     return { template, statistics, errors, warnings }
   })
 }
