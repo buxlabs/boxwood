cf16889b4a8ff1abff56115558cb093b7f2121a1
Emil Ajdyna <emil@ajdyna.com>
2020-03-15 23:25:46 +0100

    Move script handling into a separate file

--- src/tags/script.js
+++ src/tags/script.js
@@ -1,20 +1,154 @@
+const AbstractSyntaxTree = require('abstract-syntax-tree')
+const serialize = require('asttv')
+const Bundler = require('../Bundler')
 const { getLiteral } = require('../utilities/ast')
 const { getTemplateAssignmentExpression } = require('../utilities/factory')
+const { getScopeProperties } = require('../utilities/scope')
+const { findAsset } = require('../utilities/files')
+let asyncCounter = 0
 
-module.exports = function ({ tree, fragment, options }) {
-  let content = `<script`
-  fragment.attributes.forEach(attribute => {
-    if (attribute.value) {
-      content += ` ${attribute.key}="${attribute.value}"`
+module.exports = async function ({ tree, keys, attrs, fragment, assets, variables, promises, warnings, options }) {
+  if (keys.includes('inline') || options.inline.includes('scripts')) {
+    if (keys.includes('src')) {
+      const { value: path } = attrs.find(attr => attr.key === 'src')
+      const asset = findAsset(path, assets, options)
+      if (!asset) return
+      let content = '<script'
+      fragment.attributes.forEach(attribute => {
+        const { key, value } = attribute
+        if (key !== 'src' && key !== 'inline') {
+          content += ` ${key}="${value}"`
+        }
+      })
+      content += '>'
+      content += asset.source.trim()
+      content += '</script>'
+      tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral(content)))
     } else {
-      content += ` ${attribute.key}`
+      const leaf = fragment.children[0]
+      leaf.used = true
+      const ast = new AbstractSyntaxTree(leaf.content)
+      ast.each('VariableDeclarator', node => variables.push(node.id.name))
+      ast.body.forEach(node => tree.append(node))
     }
-  })
-  content += '>'
-  fragment.children.forEach(node => {
-    node.used = true
-    content += node.content
-  })
-  content += `</script>`
-  tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral(content)))
+  } else if (keys.includes('polyfills')) {
+    let content = '<script>'
+    const { value } = attrs.find(attr => attr.key === 'polyfills')
+    const ast = new AbstractSyntaxTree(value)
+    const polyfills = serialize(ast.body[0].expression)
+    polyfills.forEach(polyfill => {
+      const asset = findAsset(polyfill, assets, options)
+      if (asset) {
+        content += asset.source
+      } else {
+        warnings.push({ type: 'POLYFILL_NOT_FOUND', message: `${polyfill} polyfill not found` })
+      }
+    })
+    fragment.children.forEach(node => {
+      node.used = true
+      content += node.content
+    })
+    content += '</script>'
+    tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral(content)))
+  } else if (keys.includes('scoped')) {
+    const leaf = fragment.children[0]
+    if (!leaf) return
+    const scope = new AbstractSyntaxTree(leaf.content)
+    const properties = getScopeProperties(scope)
+    leaf.used = true
+    tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral('<script>')))
+    if (properties.length > 0) {
+      tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral('const scope = ')))
+      tree.append(getTemplateAssignmentExpression(options.variables.template, {
+        type: 'CallExpression',
+        callee: {
+          type: 'MemberExpression',
+          object: {
+            type: 'Identifier',
+            name: 'JSON'
+          },
+          property: {
+            type: 'Identifier',
+            name: 'stringify'
+          },
+          computed: false
+        },
+        arguments: [
+          {
+            type: 'ObjectExpression',
+            properties
+          }
+        ]
+      }))
+      tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral(';')))
+    }
+    asyncCounter += 1
+    const ASYNC_PLACEHOLDER_TEXT = `ASYNC_PLACEHOLDER_${asyncCounter}`
+    tree.append(getLiteral(ASYNC_PLACEHOLDER_TEXT))
+    const bundler = new Bundler()
+    const promise = bundler.bundle(leaf.content, { paths: options.script.paths, resolve: options.script.resolve })
+    promises.push(promise)
+    const result = await promise
+    tree.walk((node, parent) => {
+      if (node.type === 'Literal' && node.value === ASYNC_PLACEHOLDER_TEXT) {
+        const index = parent.body.findIndex(element => {
+          return element.type === 'Literal' && node.value === ASYNC_PLACEHOLDER_TEXT
+        })
+        parent.body.splice(index, 1)
+        parent.body.splice(index + 0, 0, getTemplateAssignmentExpression(options.variables.template, getLiteral(`\n${result}`)))
+        parent.body.splice(index + 1, 0, getTemplateAssignmentExpression(options.variables.template, getLiteral('</script>')))
+      }
+    })
+  } else if (keys.includes('compiler')) {
+    const { value } = attrs.find(attr => attr.key === 'compiler')
+    const compiler = options.compilers[value]
+    if (typeof compiler === 'function') {
+      const attr = attrs.find(attr => attr.key === 'options')
+      let params
+      if (attr && attr.value) {
+        params = JSON.parse(attr.value)
+      }
+      const leaf = fragment.children[0]
+      leaf.used = true
+      const result = compiler(leaf.content, params)
+      if (typeof result === 'string') {
+        tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral('<script>')))
+        tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral(result)))
+        tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral('</script>')))
+      } else if (result instanceof Promise) {
+        asyncCounter += 1
+        const ASYNC_PLACEHOLDER_TEXT = `ASYNC_PLACEHOLDER_${asyncCounter}`
+        tree.append(getLiteral(ASYNC_PLACEHOLDER_TEXT))
+        promises.push(result)
+        const source = await result
+        tree.walk((node, parent) => {
+          if (node.type === 'Literal' && node.value === ASYNC_PLACEHOLDER_TEXT) {
+            const index = parent.body.findIndex(element => {
+              return element.type === 'Literal' && node.value === ASYNC_PLACEHOLDER_TEXT
+            })
+            parent.body.splice(index, 1)
+            parent.body.splice(index + 0, 0, getTemplateAssignmentExpression(options.variables.template, getLiteral('<script>')))
+            parent.body.splice(index + 1, 0, getTemplateAssignmentExpression(options.variables.template, getLiteral(source)))
+            parent.body.splice(index + 2, 0, getTemplateAssignmentExpression(options.variables.template, getLiteral('</script>')))
+          }
+        })
+      }
+    }
+  } else {
+    let content = '<script'
+    fragment.attributes.forEach(attribute => {
+      if (attribute.value) {
+        content += ` ${attribute.key}="${attribute.value}"`
+      } else {
+        content += ` ${attribute.key}`
+      }
+    })
+    content += '>'
+    fragment.children.forEach(node => {
+      node.used = true
+      content += node.content
+    })
+    content += '</script>'
+    tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral(content)))
+  }
 }
--- src/utilities/collect.js
+++ src/utilities/collect.js
@@ -1,5 +1,4 @@
 const AbstractSyntaxTree = require('abstract-syntax-tree')
-const serialize = require('asttv')
 const { getLiteral } = require('./ast')
 const { getTemplateAssignmentExpression, getObjectMemberExpression } = require('./factory')
 const { convertText, convertTag } = require('./convert')
@@ -11,13 +10,10 @@ const { inlineAttributesInIfStatement, inlineLocalVariablesInFragment, inlineExp
 const { clone } = require('./object')
 const { isImportTag } = require('./string')
 const Component = require('../Component')
-const Bundler = require('../Bundler')
 const tags = require('../tags')
 const { hasShorthandSyntax, normalizeAttributes } = require('./node')
 const { getComponentNames } = require('./attributes')
 const { findAsset } = require('./files')
-const { getScopeProperties } = require('./scope')
-let asyncCounter = 0
 
 function collectComponentsFromImport (fragment, components, context, assets, options) {
   const attrs = fragment.attributes
@@ -177,10 +173,7 @@ function resolveComponent (content, path, component, fragment, components, plugi
       collectComponentsFromPartialOrRender(current, assets, path, plugins, errors, options)
     } else if (current.attributes && current.attributes[0] && current.attributes[0].key === 'partial') {
       collectComponentsFromPartialAttribute(current, assets, path, plugins, errors, options)
-    } else if (
-      (current.tagName === 'template' && keys.length > 0) ||
-      (current.tagName === 'script' && keys.includes('component'))
-    ) {
+    } else if (current.tagName === 'template' && keys.length > 0) {
       collectInlineComponents(current, attrs, currentComponents)
     }
     const currentComponent = currentComponents.find(component => component.name === current.tagName)
@@ -297,136 +290,8 @@ async function collect ({ source, tree, fragment, assets, variables, filters, co
       fragment.children.forEach(child => {
         child.used = true
       })
-    } else if (
-      (tag === 'template' && keys.length > 0) ||
-      (tag === 'script' && keys.includes('component'))
-    ) {
+    } else if (tag === 'template' && keys.length > 0) {
       collectInlineComponents(fragment, attrs, components)
-    } else if ((tag === 'script' && keys.includes('inline')) || options.inline.includes('scripts')) {
-      if (keys.includes('src')) {
-        const { value: path } = attrs.find(attr => attr.key === 'src')
-        const asset = findAsset(path, assets, options)
-        if (!asset) return
-        let content = '<script'
-        fragment.attributes.forEach(attribute => {
-          const { key, value } = attribute
-          if (key !== 'src' && key !== 'inline') {
-            content += ` ${key}="${value}"`
-          }
-        })
-        content += '>'
-        content += asset.source.trim()
-        content += '</script>'
-        tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral(content)))
-      } else {
-        const leaf = fragment.children[0]
-        leaf.used = true
-        const ast = new AbstractSyntaxTree(leaf.content)
-        ast.each('VariableDeclarator', node => variables.push(node.id.name))
-        ast.body.forEach(node => tree.append(node))
-      }
-    } else if (tag === 'script' && keys.includes('scoped')) {
-      const leaf = fragment.children[0]
-      const scope = new AbstractSyntaxTree(leaf.content)
-      const properties = getScopeProperties(scope)
-      leaf.used = true
-      tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral('<script>')))
-      if (properties.length > 0) {
-        tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral('const scope = ')))
-        tree.append(getTemplateAssignmentExpression(options.variables.template, {
-          type: 'CallExpression',
-          callee: {
-            type: 'MemberExpression',
-            object: {
-              type: 'Identifier',
-              name: 'JSON'
-            },
-            property: {
-              type: 'Identifier',
-              name: 'stringify'
-            },
-            computed: false
-          },
-          arguments: [
-            {
-              type: 'ObjectExpression',
-              properties
-            }
-          ]
-        }))
-        tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral(';')))
-      }
-      asyncCounter += 1
-      const ASYNC_PLACEHOLDER_TEXT = `ASYNC_PLACEHOLDER_${asyncCounter}`
-      tree.append(getLiteral(ASYNC_PLACEHOLDER_TEXT))
-      const bundler = new Bundler()
-      const promise = bundler.bundle(leaf.content, { paths: options.script.paths, resolve: options.script.resolve })
-      promises.push(promise)
-      const result = await promise
-      tree.walk((node, parent) => {
-        if (node.type === 'Literal' && node.value === ASYNC_PLACEHOLDER_TEXT) {
-          const index = parent.body.findIndex(element => {
-            return element.type === 'Literal' && node.value === ASYNC_PLACEHOLDER_TEXT
-          })
-          parent.body.splice(index, 1)
-          parent.body.splice(index + 0, 0, getTemplateAssignmentExpression(options.variables.template, getLiteral(`\n${result}`)))
-          parent.body.splice(index + 1, 0, getTemplateAssignmentExpression(options.variables.template, getLiteral('</script>')))
-        }
-      })
-    } else if (tag === 'script' && keys.includes('polyfills')) {
-      let content = '<script>'
-      const { value } = attrs.find(attr => attr.key === 'polyfills')
-      const ast = new AbstractSyntaxTree(value)
-      const polyfills = serialize(ast.body[0].expression)
-      polyfills.forEach(polyfill => {
-        const asset = findAsset(polyfill, assets, options)
-        if (asset) {
-          content += asset.source
-        } else {
-          warnings.push({ type: 'POLYFILL_NOT_FOUND', message: `${polyfill} polyfill not found` })
-        }
-      })
-      fragment.children.forEach(node => {
-        node.used = true
-        content += node.content
-      })
-      content += '</script>'
-      tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral(content)))
-    } else if (tag === 'script' && keys.includes('compiler')) {
-      const { value } = attrs.find(attr => attr.key === 'compiler')
-      const compiler = options.compilers[value]
-      if (typeof compiler === 'function') {
-        const attr = attrs.find(attr => attr.key === 'options')
-        let params
-        if (attr && attr.value) {
-          params = JSON.parse(attr.value)
-        }
-        const leaf = fragment.children[0]
-        leaf.used = true
-        const result = compiler(leaf.content, params)
-        if (typeof result === 'string') {
-          tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral('<script>')))
-          tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral(result)))
-          tree.append(getTemplateAssignmentExpression(options.variables.template, getLiteral('</script>')))
-        } else if (result instanceof Promise) {
-          asyncCounter += 1
-          const ASYNC_PLACEHOLDER_TEXT = `ASYNC_PLACEHOLDER_${asyncCounter}`
-          tree.append(getLiteral(ASYNC_PLACEHOLDER_TEXT))
-          promises.push(result)
-          const source = await result
-          tree.walk((node, parent) => {
-            if (node.type === 'Literal' && node.value === ASYNC_PLACEHOLDER_TEXT) {
-              const index = parent.body.findIndex(element => {
-                return element.type === 'Literal' && node.value === ASYNC_PLACEHOLDER_TEXT
-              })
-              parent.body.splice(index, 1)
-              parent.body.splice(index + 0, 0, getTemplateAssignmentExpression(options.variables.template, getLiteral('<script>')))
-              parent.body.splice(index + 1, 0, getTemplateAssignmentExpression(options.variables.template, getLiteral(source)))
-              parent.body.splice(index + 2, 0, getTemplateAssignmentExpression(options.variables.template, getLiteral('</script>')))
-            }
-          })
-        }
-      }
     } else if (tag === 'link' && (keys.includes('inline') || options.inline.includes('stylesheets'))) {
       const { value: path } = attrs.find(attr => attr.key === 'href')
       const asset = findAsset(path, assets, options)
@@ -435,7 +300,7 @@ async function collect ({ source, tree, fragment, assets, variables, filters, co
     } else if (tag === 'style') {
       tags.style({ fragment, styles })
     } else if (tag === 'script') {
-      tags.script({ tree, fragment, options })
+      tags.script({ tree, fragment, keys, attrs, assets, variables, promises, warnings, options })
     } else if (tag === 'template') {
       tags.template({ tree, fragment, options })
     } else if (tag === '!doctype') {
