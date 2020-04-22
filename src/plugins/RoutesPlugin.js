242f44a1f9822b363e157d24029a603b9a77fba3
Emil Ajdyna <emil@ajdyna.com>
2020-04-22 23:31:11 +0200

    Don't run if the routes are not defined

--- src/plugins/RoutesPlugin.js
+++ src/plugins/RoutesPlugin.js
@@ -1,5 +1,16 @@
 const Plugin = require('./Plugin')
+const { isEmptyObject } = require('pure-conditions')
 
-class RoutesPlugin extends Plugin {}
+class RoutesPlugin extends Plugin {
+  constructor (options) {
+    super()
+    const { routes } = options
+    this.routes = routes
+    this.disabled = isEmptyObject(routes)
+  }
+  prerun () {
+    if (this.disabled) { return }
+  }
+}
 
 module.exports = RoutesPlugin
