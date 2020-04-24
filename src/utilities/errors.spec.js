eb5e2a3fccee42bd860721d75fcc275810eb3a4d
Emil Ajdyna <emil@ajdyna.com>
2020-04-24 22:04:11 +0200

    Rename normalizeErrors to normalizeError

--- src/utilities/errors.spec.js
+++ src/utilities/errors.spec.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const test = require('ava')
-const { normalizeErrors } = require('./errors')
+const { normalizeError } = require('./errors')
 
 test('it returns an empty string if the error has no stack trace', assert => {
   const input = { type: 'FOO_BAZ', message: 'bar' }
-  const output = normalizeErrors(input)
+  const output = normalizeError(input)
   assert.deepEqual(output, { type: 'FOO_BAZ', message: 'bar', stack: '' })
 })
