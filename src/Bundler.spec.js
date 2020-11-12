51149a57adc551b331258698bd7554954cab82df
Emil Ajdyna <emil@ajdyna.com>
2020-11-12 23:51:47 +0100

    Set target

--- src/Bundler.spec.js
+++ src/Bundler.spec.js
@@ -8,7 +8,7 @@ test('Bundler: keeps simple code', async assert => {
   const source = 'const foo = "bar"; console.log(foo)'
   const bundler = new Bundler()
   const output = await bundler.bundle(source)
-  assert.truthy(output.includes(source))
+  assert.truthy(output.includes('console.log'))
 })
 
 test('Bundler: can import local dependencies', async assert => {
