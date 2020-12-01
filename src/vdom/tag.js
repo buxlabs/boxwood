505da621a174346b912df209b3bbd472da9beb4f
Emil Ajdyna <emil@ajdyna.com>
2020-12-02 00:11:35 +0100

    Check empty tag case

--- src/vdom/tag.js
+++ src/vdom/tag.js
@@ -2,11 +2,40 @@
 
 const node = require('./node')
 
-function tag (name, attributes = {}, children = []) {
+function toArray (children) {
+  if (Array.isArray(children)) {
+    return children
+  }
+  return [children]
+}
+
+function normalize (params) {
+  if (params.length === 3) {
+    const [name, attributes, children] = params
+    return [name, attributes, children]
+  }
+
+  if (params.length === 2) {
+    if (Array.isArray(params[1]) || typeof params[1] === 'string') {
+      const [name, children] = params
+      return [name, {}, children]
+    }
+    const [name, attributes] = params
+    return [name, attributes, []]
+  }
+  if (params.length === 1) {
+    const [name] = params
+    return [name, {}, []]
+  }
+  return ['div', {}, []]
+}
+
+function tag () {
+  const [name, attributes, children] = normalize([...arguments])
   return node({
     name,
     attributes,
-    children
+    children: toArray(children)
   })
 }
 
