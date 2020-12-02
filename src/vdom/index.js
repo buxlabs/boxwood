e0fd0f6158ede45b41d5d42dcc6ff76e031dd116
Emil Ajdyna <emil@ajdyna.com>
2020-12-02 21:53:37 +0100

    Limit the api

--- src/vdom/index.js
+++ src/vdom/index.js
@@ -1,17 +1,11 @@
 'use strict'
 
 const tag = require('./tag')
-const mount = require('./mount')
-const render = require('./render')
-const diff = require('./diff')
 const app = require('./app')
 const nodes = require('./nodes')
 
 module.exports = {
   ...nodes,
   app,
-  mount,
-  render,
-  diff,
   tag
 }
