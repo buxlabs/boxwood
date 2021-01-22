97c81302b611b17cb1ab8f61366ba297f9df668f
Emil Ajdyna <emil@ajdyna.com>
2021-01-22 22:00:02 +0100

    Add an additional spec

--- src/plugins/ScopedStylesPlugin/css.spec.js
+++ src/plugins/ScopedStylesPlugin/css.spec.js
@@ -1,7 +1,12 @@
 const test = require('ava')
 const { addScopeToCssSelectors } = require('./css')
 
-test('#addScopeToCssSelectors: it adds a scope based on a hash', assert => {
+test('#addScopeToCssSelectors: it works for classes', assert => {
   const result = addScopeToCssSelectors('.foo { color: red }', [])
   assert.deepEqual(result, '.scope-2771010719.foo{color:red}')
+})
+
+test('#addScopeToCssSelectors: it works for tags', assert => {
+  const result = addScopeToCssSelectors('div { color: red }', [])
+  assert.deepEqual(result, 'div.scope-2947263116{color:red}')
 })
