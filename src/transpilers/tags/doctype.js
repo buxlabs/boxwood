4bc3ca447d0d1e97743c9def9714b569a662f9c2
Emil Ajdyna <emil@ajdyna.com>
2021-04-06 23:18:23 +0200

    Simplify the doctype declaration

--- src/transpilers/tags/doctype.js
+++ src/transpilers/tags/doctype.js
@@ -1,29 +1,6 @@
 const AbstractSyntaxTree = require('abstract-syntax-tree')
 
-const { CallExpression, Identifier, Literal, ObjectExpression, Property } = AbstractSyntaxTree
-
 module.exports = function doctype () {
-  const node = new CallExpression({
-    callee: new Identifier({ name: 'tag' }),
-    arguments: [
-      new Literal({ value: '!DOCTYPE' }),
-      new ObjectExpression({
-        properties: [
-          new Property({
-            key: new Identifier({
-              name: 'html'
-            }),
-            value: new Literal({
-              value: true
-            }),
-            kind: 'init',
-            computed: false,
-            method: false,
-            shorthand: false
-          })
-        ]
-      })
-    ]
-  })
-  return node
+  const [node] = AbstractSyntaxTree.template('tag("!DOCTYPE", { html: true })', {})
+  return node.expression
 }
