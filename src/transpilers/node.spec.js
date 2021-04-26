d4cdfd3dcd3dbec73df908f149aebb1dcc69ec54
Emil Ajdyna <emil@ajdyna.com>
2021-04-26 22:10:26 +0200

    Add a spec for multiple nodes

--- src/transpilers/node.spec.js
+++ src/transpilers/node.spec.js
@@ -1,18 +1,31 @@
 'use strict'
 
 const test = require('ava')
-const { program, generate } = require('abstract-syntax-tree')
+const { program, generate, ArrayExpression } = require('abstract-syntax-tree')
 const { transpileNode } = require('./node')
 const { parse } = require('../utilities/html')
 
+function body (tree) {
+  return tree.length === 1
+    ? transpileNode({ node: tree[0], parent: tree, index: 0 })
+    : new ArrayExpression(
+      tree
+        .map((node, index) => transpileNode({ node, parent: tree, index }))
+        .filter(Boolean)
+    )
+}
+
 function transpile (input) {
-  const htmlTree = parse(input)
-  const jsTree = transpileNode({ node: htmlTree[0], parent: htmlTree, index: 0 })
-  return generate(program(jsTree)).trim()
+  const tree = parse(input)
+  return generate(program(body(tree))).trim()
 }
 
 test('transpileNode: div', assert => {
   assert.deepEqual(transpile('<div></div>'), 'tag("div")')
+})
+
+test('transpileNode: multiple divs', assert => {
+  assert.deepEqual(transpile('<div></div><div></div>'), '[tag("div"), tag("div")]')
 })
 
 test('transpileNode: br', assert => {
