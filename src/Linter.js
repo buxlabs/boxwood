e65b5b6f9422ce216d86418551b4d25ba4c2a82a
Emil Ajdyna <emil@ajdyna.com>
2021-07-19 21:02:03 +0200

    Simplify the linter

--- src/Linter.js
+++ src/Linter.js
@@ -35,9 +35,9 @@ module.exports = class Linter {
     const ANCHOR_TAG = 'a'
     const IMAGE_TAG = 'img'
     const warnings = []
-    const data = analyze(tree)
+    const { components } = analyze(tree)
     walk(tree, node => {
-      if (node.tagName === ANCHOR_TAG && !data.components.includes(ANCHOR_TAG)) {
+      if (node.tagName === ANCHOR_TAG && !components.includes(ANCHOR_TAG)) {
         const href = node.attributes.find(attribute => attribute.key === 'href')
         if (href && isExternalUrl(href.value)) {
           const rel = node.attributes.find(attribute => attribute.key === 'rel')
@@ -47,7 +47,7 @@ module.exports = class Linter {
         }
       }
 
-      if (node.tagName === IMAGE_TAG && !data.components.includes(IMAGE_TAG)) {
+      if (node.tagName === IMAGE_TAG && !components.includes(IMAGE_TAG)) {
         const alt = node.attributes.find(attribute => attribute.key === 'alt')
         if (!alt) {
           warnings.push({ message: `${IMAGE_TAG} tag should have an alt attribute`, type: 'ALT_ATTRIBUTE_MISSING' })
@@ -59,15 +59,15 @@ module.exports = class Linter {
 
   verifyComponents (tree) {
     const warnings = []
-    const data = analyze(tree)
+    const { components } = analyze(tree)
     walk(tree, node => {
-      const index = data.components.indexOf(node.tagName)
+      const index = components.indexOf(node.tagName)
       if (index !== -1) {
-        data.components.splice(index, 1)
+        components.splice(index, 1)
       }
     })
     // TODO: unify warnings.
-    data.components.forEach(component => {
+    components.forEach(component => {
       warnings.push({ type: 'UNUSED_COMPONENT', message: `${component} component is unused` })
     })
     return warnings
