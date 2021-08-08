91e99e2a76ae9816012edfdc606d788fd1be4737
Emil Ajdyna <emil@ajdyna.com>
2021-08-08 21:27:14 +0200

    Transpile type selectors

--- src/transpilers/css/index.js
+++ src/transpilers/css/index.js
@@ -2,16 +2,33 @@
 
 const { parse, walk, generate } = require('css-tree')
 const createHash = require('string-hash')
+const { camelize } = require('pure-utilities/string')
 
 function transpile (source) {
   const hash = createHash(source)
   const tree = parse(source)
   walk(tree, node => {
     if (node.type === 'ClassSelector') {
-      node.name = `${node.name}-${hash}`
+      node.name = `${camelize(node.name)}-${hash}`
+    }
+    if (node.type === 'TypeSelector') {
+      node.type = 'ClassSelector'
+      node.name = `${camelize(node.name)}-${hash}`
     }
   })
   return generate(tree)
 }
 
-module.exports = { transpile }
+function getSelectors (source) {
+  const tree = parse(source)
+  const selectors = {}
+  walk(tree, node => {
+    if (node.type === 'ClassSelector') {
+      const [key] = node.name.split('-')
+      selectors[key] = node.name
+    }
+  })
+  return selectors
+}
+
+module.exports = { transpile, getSelectors }
--- src/transpilers/css/index.spec.js
+++ src/transpilers/css/index.spec.js
@@ -1,8 +1,23 @@
 'use strict'
 
 const test = require('ava')
-const { transpile } = require('.')
+const { transpile, getSelectors } = require('.')
 
-test('transpile: simple class', assert => {
-  assert.deepEqual(transpile('.foo { color: red }'), `.foo-2771010719{color:red}`)
+test('transpile: class selector', assert => {
+  assert.deepEqual(transpile('.foo { color: red }'), '.foo-2771010719{color:red}')
+})
+
+test('transpile: class selector with a hyphen', assert => {
+  assert.deepEqual(transpile('.foo-bar { color: red }'), '.fooBar-1294733379{color:red}')
+})
+
+test('transpile: type selector', assert => {
+  assert.deepEqual(transpile('h3 { color: red }'), '.h3-1911416268{color:red}')
+})
+
+test('getSelectors: class selector', assert => {
+  assert.deepEqual(getSelectors('.foo-2771010719{color:red}'), { foo: 'foo-2771010719' })
+})
+test('getSelectors: class selector with a camelized name', assert => {
+  assert.deepEqual(getSelectors('.fooBar-1294733379{color:red}'), { fooBar: 'fooBar-1294733379' })
 })
