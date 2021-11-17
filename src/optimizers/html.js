b409b51c92d1ab25d114e157105d19ca3dc1ca30
Emil Ajdyna <emil@ajdyna.com>
2021-11-17 21:05:24 +0100

    Add a html-minifier

--- src/optimizers/html.js
+++ src/optimizers/html.js
@@ -1,5 +1,13 @@
+const { minify } = require('html-minifier')
+
 function optimize (html) {
-  return html.trim()
+  return minify(html.trim(), {
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    removeComments: true,
+    sortAttributes: true,
+    sortClassName: true
+  })
 }
 
 module.exports = { optimize }
--- src/optimizers/html.spec.js
+++ src/optimizers/html.spec.js
@@ -4,3 +4,19 @@ const { optimize } = require('./html')
 test('optimize: trims html', assert => {
   assert.deepEqual(optimize('  <div></div>   '), '<div></div>')
 })
+
+test('optimize: collapses whitespaces conservatively', assert => {
+  assert.deepEqual(optimize('   <div>    </div>   '), '<div> </div>')
+})
+
+test('optimize: removes comments', assert => {
+  assert.deepEqual(optimize('<!-- foo -->'), '')
+})
+
+test('optimize: sorts attributes', assert => {
+  assert.deepEqual(optimize('<div id="foo" class="bar"></div>'), '<div class="bar" id="foo"></div>')
+})
+
+test('optimize: sorts classes', assert => {
+  assert.deepEqual(optimize('<div class="foo bar"></div>'), '<div class="bar foo"></div>')
+})
