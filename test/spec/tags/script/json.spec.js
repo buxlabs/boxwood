a9d9c6226ecd00a0891540b51021d2ccdcd45c23
Emil Ajdyna <emil@ajdyna.com>
2022-02-21 14:21:27 +0100

    Migrate the json spec

--- test/spec/tags/script/json.spec.js
+++ test/spec/tags/script/json.spec.js
@@ -1,5 +1,5 @@
 const test = require('ava')
-const compile = require('../../../helpers/deprecated-compile')
+const compile = require('../../../helpers/compile')
 const { escape } = require('../../../..')
 
 test('script[type="application/json"]', async assert => {
