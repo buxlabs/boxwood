2a1d7ed8d77abb3ed1280df1dc6f06fc0456d3a0
Emil Ajdyna <emil@ajdyna.com>
2022-08-25 08:33:21 +0200

    Remove rollup

--- src/Bundler.js
+++ src/Bundler.js
@@ -1,12 +1,10 @@
 'use strict'
 
 const esbuild = require('./bundlers/esbuild')
-const rollup = require('./bundlers/rollup')
 
 class Bundler {
   async bundle (source, options = {}) {
-    const bundler = options.bundler === 'rollup' ? rollup : esbuild
-    return bundler.bundle(source, options)
+    return esbuild.bundle(source, options)
   }
 }
 
