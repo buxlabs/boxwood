ffebc22e4a99031ad25e4cfc06b8fa931932cc91
Emil Ajdyna <emil@ajdyna.com>
2022-08-28 22:55:57 +0200

    Ensure that variables can be passed to the template

--- test/spec/compile.spec.js
+++ test/spec/compile.spec.js
@@ -8,3 +8,21 @@ test('compile: it returns a template', async assert => {
 
   assert.deepEqual(template(), "foo")
 })
+
+test('compile: it returns a template which can have parameters', async assert => {
+  const { template } = await compile(`
+    module.exports = ({ foo }) => foo
+  `)
+
+  assert.deepEqual(template({ foo: 'bar' }), "bar")
+})
+
+test('compile: it returns a template which can require dependencies', async assert => {
+  const { template } = await compile(`
+    const { join } = require('path')
+
+    module.exports = () => join('foo', 'bar')
+  `)
+
+  assert.deepEqual(template(), 'foo/bar')
+})
