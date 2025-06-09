1e7d02be143edcb69bb2fd5d581530cf8542fc38
Emil Ajdyna <emilajdyna@gmail.com>
2025-06-09 23:20:11 +0200

    Fix test description

--- test/raw/load/index.test.js
+++ test/raw/load/index.test.js
@@ -3,7 +3,7 @@ const assert = require("node:assert")
 const { join } = require("path")
 const { compile } = require("../../..")
 
-test("#svg: it returns an svg", () => {
+test("#raw.load: can load an html file", () => {
   const { template } = compile(join(__dirname, "./index.js"))
   assert(template().includes("<li>foo</li>"))
   assert(template().includes("<li>bar</li>"))
