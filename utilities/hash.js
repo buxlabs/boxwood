1b75c7b7d652bc01e3e6c31fb2acfc8a1891bab7
emilos <emilajdyna@gmail.com>
2025-10-03 17:43:48 +0200

    Add a global index to prevent collisions for multiple instances

--- utilities/hash.js
+++ utilities/hash.js
@@ -1,12 +1,16 @@
-let index = 0
+if (!global.__boxwood_index__) {
+  global.__boxwood_index__ = 0
+}
+
 const map = new Map()
 
 function createHash(string) {
   if (map.has(string)) {
     return map.get(string)
   }
-  index++
-  const hash = "c" + index
+
+  global.__boxwood_index__++
+  const hash = "c" + global.__boxwood_index__
   map.set(string, hash)
   return hash
 }
